2a437c9adc1cfb122fb00e2c963982a08325202c
dni <dni.khr@gmail.com>
2022-07-09 18:11:24 +0200

    add tags, fix createeffect

--- src/App.jsx
+++ src/App.jsx
@@ -2,56 +2,60 @@ import { createSignal, createEffect } from "solid-js";
 import { render } from "solid-js/web";
 import fetcher from "./fetcher";
 
+const startInterval = (cb) => {
+  cb();
+  return setInterval(cb, 10000);
+};
+
 const App = () => {
   const [boltzFee, setBoltzFee] = createSignal(0);
   const [minerFee, setMinerFee] = createSignal(0);
   const [minimum, setMinimum] = createSignal(0);
   const [maximum, setMaximum] = createSignal(0);
-  const [amount, setAmount] = createSignal(0);
-  const [amount2, setAmount2] = createSignal(0);
-  const [reverse, setReverse] = createSignal(0);
+  const [sendAmount, setSendAmount] = createSignal(0);
+  const [receiveAmount, setReceiveAmount] = createSignal(0);
+  const [reverse, setReverse] = createSignal(false);
   const [config, setConfig] = createSignal(0);
 
-  let factor = 0.00000001;
+  let divider = 100000000;
 
-  fetcher("/getpairs", (data) => {
-    let cfg = data.pairs["BTC/BTC"];
-    setConfig(cfg);
-    setMinimum(cfg.limits.minimal * factor);
-    setMaximum(cfg.limits.maximal * factor);
-    setMinerFee(cfg.fees.minerFees.baseAsset.normal * factor);
-    setBoltzFee(cfg.fees.percentage);
-    setAmount(cfg.limits.maximal * factor);
+  setSendAmount(0.05);
+
+  startInterval(() => {
+    fetcher("/getpairs", (data) => {
+      let cfg = data.pairs["BTC/BTC"];
+      setConfig(cfg);
+    });
   });
-
 
   createEffect(() => {
-    setAmount2(amount() - amount() * boltzFee() / 100);
+    let cfg = config()
+    if (cfg) {
+      setMinimum(cfg.limits.minimal / divider);
+      setMaximum(cfg.limits.maximal / divider);
+      setBoltzFee(cfg.fees.percentage);
+      setReceiveAmount(sendAmount() - sendAmount() * boltzFee() / 100);
+      if (reverse()) {
+        let rev = cfg.fees.minerFees.baseAsset.reverse;
+        let fee = rev.claim  + rev.lockup;
+        setMinerFee(fee / divider);
+      } else {
+        setMinerFee(cfg.fees.minerFees.baseAsset.normal / divider);
+      }
+    }
   });
-
-  // createEffect(() => {
-  //   let cfg = config();
-  //   if (reverse()) {
-  //     let rev = cfg.fees.minerFees.baseAsset.reverse;
-  //     let fee = rev.claim  + rev.lockUp;
-  //     setMinerFee(fee * factor);
-  //   } else {
-  //     setMinerFee(cfg.fees.minerFees.baseAsset.normal * factor);
-  //   }
-
-  // });
 
   return (
     <div class="container">
-      <form action="#">
+      <form action="#" data-reverse={reverse()}>
         <div>
-          <input type="text" id="amount" value={amount()} onChange={(e) => setAmount(e.currentTarget.value)} />
+          <input type="text" id="sendAmount" value={sendAmount()} onKeyUp={(e) => setSendAmount(e.currentTarget.value)} />
         </div>
         <div>
-          <input type="checkbox" id="reverse" value={reverse()} onChange={(e) => setReverse(e.currentTarget.value)} />
+          <input type="checkbox" id="reverse" value={reverse()} onChange={(e) => setReverse(e.currentTarget.checked)} />
         </div>
         <div>
-          <input type="text" id="amount2" value={amount2()} onChange={(e) => setAmount2(e.currentTarget.value)} />
+          <span id="receiveAmount">{receiveAmount()} BTC</span>
         </div>
       </form>
       <div class="fees">
@@ -70,6 +74,29 @@ const App = () => {
         <div class="fee">
           <span><b>{minerFee()} BTC</b></span><br />
           <label>Miner fee</label>
+        </div>
+      </div>
+      <div class="tags">
+        <div class="tag">
+          <span class="btn" onClick={(e) => setSendAmount(0.0001)}>100K</span>
+        </div>
+        <div class="tag">
+          <span class="btn" onClick={(e) => setSendAmount(0.0005)}>500K</span>
+        </div>
+        <div class="tag">
+          <span class="btn" onClick={(e) => setSendAmount(0.001)}>1M</span>
+        </div>
+        <div class="tag">
+          <span class="btn" onClick={(e) => setSendAmount(0.003)}>3M</span>
+        </div>
+        <div class="tag">
+          <span class="btn" onClick={(e) => setSendAmount(0.05)}>5M</span>
+        </div>
+        <div class="tag">
+          <span class="btn" onClick={(e) => setSendAmount(0.007)}>7M</span>
+        </div>
+        <div class="tag">
+          <span class="btn" onClick={(e) => setSendAmount(0.1)}>10M</span>
         </div>
       </div>
     </div>
