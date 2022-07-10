4921338c1b796c6b0d59f9b201afc13549aa3460
dni <dni.khr@gmail.com>
2022-07-10 12:04:14 +0200

    add rainbow mode

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,14 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
 import App from './App';
+
+// rainbow mode
+document.getElementById("toggle-rainbow").addEventListener("click", (e) => {
+  if (document.body.classList.contains("rainbow")) {
+    document.body.classList.remove("rainbow")
+  } else {
+    document.body.classList.add("rainbow")
+  }
+});
+
 render(() => <App />, document.getElementById('root'));
