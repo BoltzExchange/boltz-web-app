34504b50e4d5f191360eab1c58d18653a757fad5
dni ⚡ <office@dnilabs.com>
2023-02-19 16:03:44 +0100

    add cname and fix links

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -12,13 +12,13 @@ const Nav = () => {
           <path class="line bottom" d="m 30,67 h 40 c 0,0 8.5,0.149796 8.5,-8.5 0,-8.649796 -8.5,-8.5 -8.5,-8.5 h -20 v 20" />
       </svg>
 
-      <A id="logo" href="swap">
+      <A id="logo" href="/">
         <img src={logo} alt="boltz exchange btc logo" />
       </A>
 
       <div class="collapse">
-          <A href="swap">Swap</A>
-          <A href="refund">Refund</A>
+          <A href="/swap">Swap</A>
+          <A href="/refund">Refund</A>
           <a
             class="external"
             target="_blank"
