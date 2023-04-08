3176b42dbb459e4ca59738f3492dd0a003741025
dni ⚡ <office@dnilabs.com>
2023-04-08 13:49:05 +0200

    testnet verbose logging

--- src/configs/config.testnet.js
+++ src/configs/config.testnet.js
@@ -2,7 +2,7 @@ import { networks } from "bitcoinjs-lib";
 export const network = "testnet";
 export const net = networks[network];
 export const bolt11_prefix = "lntb";
-export const loglevel = "info";
+export const loglevel = "debug";
 export const api_url = "https://testnet.boltz.exchange/api";
 export const mempool_url = "https://mempool.space/testnet";
 export const mempool_ws_url = "wss://mempool.space/testnet/api/v1/ws";
