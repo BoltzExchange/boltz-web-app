5adf3a7f07757711e3aee59bed453a4eed805236
michael1011 <me@michael1011.at>
2023-04-08 14:10:42 +0200

    support taproot claim addresses

--- src/ecpair/ecpair.js
+++ src/ecpair/ecpair.js
@@ -1,6 +1,8 @@
 import { ECPairFactory } from "ecpair";
+import { initEccLib } from 'bitcoinjs-lib';
 import * as ecc from './noble';
 
+initEccLib(ecc);
 const ECPair = ECPairFactory(ecc);
 
 export { ECPair };
