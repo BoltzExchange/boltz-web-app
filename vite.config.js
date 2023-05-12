a213ab9707563b193407c6278afdd50d090035b5
dni ⚡ <office@dnilabs.com>
2023-05-12 20:08:40 +0200

    fix static assets in meta

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import solidPlugin from "vite-plugin-solid";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
+  assetsInclude: ['**/boltz-preview.jpg'],
   plugins: [solidPlugin(), nodePolyfills()],
   server: {
     cors: { origin: "*" },
