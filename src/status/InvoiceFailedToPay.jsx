e6945f46e195bd5fe706943ecbaa235e1d5b1193
dni ⚡ <office@dnilabs.com>
2023-05-27 23:28:01 +0200

    fix issue with refunding on invoicefailedtopay

--- src/status/InvoiceFailedToPay.jsx
+++ src/status/InvoiceFailedToPay.jsx
@@ -1,10 +1,36 @@
+import log from "loglevel";
+import { createSignal } from "solid-js";
 import { useI18n } from "@solid-primitives/i18n";
-import { failureReason } from "../signals";
+import fetcher, { refundAddressChange } from "../helper";
+import {
+    failureReason,
+    swap,
+    timeoutEta,
+    setTransactionToRefund,
+} from "../signals";
 import RefundEta from "../components/RefundEta";
 import DownloadRefund from "../components/DownloadRefund";
 
 const InvoiceFailedToPay = () => {
     const [t] = useI18n();
+    const [valid, setValid] = createSignal(false);
+
+    createEffect(() => {
+        setTransactionToRefund(null);
+        fetcher(
+            "/getswaptransaction",
+            (res) => {
+                log.debug(`got swap transaction for ${swap().id}`);
+                setTransactionToRefund(res);
+            },
+            {
+                id: swap().id,
+            },
+            () => {
+                log.warn(`no swap transaction for: ${swap().id}`);
+            }
+        );
+    });
 
     return (
         <div>
@@ -13,7 +39,26 @@ const InvoiceFailedToPay = () => {
                 {t("failure_reason")}: {failureReason()}
             </p>
             <hr />
-            <RefundEta />
+            <Show when={timeoutEta()}>
+                <RefundEta />
+            </Show>
+            <Show when={!timeoutEta()}>
+                <input
+                    onInput={(e) =>
+                        setValid(refundAddressChange(e, swap().asset))
+                    }
+                    type="text"
+                    id="refundAddress"
+                    name="refundAddress"
+                    placeholder={t("refund_address_placeholder")}
+                />
+                <button
+                    class="btn"
+                    disabled={valid() ? "" : "disabled"}
+                    onclick={() => refund(swap())}>
+                    {t("refund")}
+                </button>
+            </Show>
             <DownloadRefund />
             <hr />
         </div>
