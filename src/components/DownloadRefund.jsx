d84efc6c1fb29f938af450251e1fd711789f5870
michael1011 <me@michael1011.at>
2023-05-30 12:02:59 +0200

    fix download of QR in Brave on iOS (#97)

--- src/components/DownloadRefund.jsx
+++ src/components/DownloadRefund.jsx
@@ -3,8 +3,9 @@ import QRCode from "qrcode";
 import { useI18n } from "@solid-primitives/i18n";
 import { swap, setNotificationType, setNotification } from "../signals";
 
-const is_mobile =
-    !!navigator.userAgent.match(/iphone|android|blackberry/gi) || false;
+const isIos = !!navigator.userAgent.match(/iphone|ipad/gi) || false;
+const isMobile =
+    isIos || !!navigator.userAgent.match(/android|blackberry/gi) || false;
 
 const createRefundData = (swap) => {
     return {
@@ -21,7 +22,7 @@ const getRefundFileName = (swap) => {
     return "boltz-refund-" + swap.id;
 };
 
-export const downloadRefundFile = (swap) => {
+const downloadRefundFile = (swap) => {
     const hiddenElement = document.createElement("a");
     hiddenElement.href =
         "data:application/json;charset=utf-8," +
@@ -31,33 +32,45 @@ export const downloadRefundFile = (swap) => {
     hiddenElement.click();
 };
 
-export const downloadRefundQr = (swap) => {
-    let hiddenElement = document.createElement("a");
-    QRCode.toDataURL(JSON.stringify(createRefundData(swap)), {
-        width: 400,
-    })
-        .then((url) => {
-            hiddenElement.href = url;
-            hiddenElement.target = "_blank";
-            hiddenElement.download = getRefundFileName(swap) + ".png";
-            hiddenElement.click();
-        })
-        .catch((err) => {
-            const msg = "error: QR code generation failed";
-            log.error(msg, err);
-            setNotificationType("error");
-            setNotification(msg);
-        });
-};
-
 const DownloadRefund = () => {
     const [t] = useI18n();
+
+    const downloadRefundQr = (swap) => {
+        QRCode.toDataURL(JSON.stringify(createRefundData(swap)), {
+            width: 400,
+        })
+            .then(async (url) => {
+                if (isIos) {
+                    // Compatibility with third party iOS browsers
+                    const newTab = window.open();
+                    newTab.document.body.innerHTML = `
+                    <!DOCTYPE html>
+                    <body>
+                        <img src="${url}">
+                        <h1>${t("ios_image_download")}</h1>
+                    </body>`;
+                } else {
+                    const hiddenElement = document.createElement("a");
+                    hiddenElement.href = url;
+                    hiddenElement.target = "_blank";
+                    hiddenElement.download = getRefundFileName(swap) + ".png";
+                    hiddenElement.click();
+                }
+            })
+            .catch((err) => {
+                const msg = "error: QR code generation failed";
+                log.error(msg, err);
+                setNotificationType("error");
+                setNotification(msg);
+            });
+    };
+
     return (
         <div className="download-refund">
             <button
                 class="btn btn-success"
                 onclick={() =>
-                    is_mobile
+                    isMobile
                         ? downloadRefundQr(swap())
                         : downloadRefundFile(swap())
                 }>
