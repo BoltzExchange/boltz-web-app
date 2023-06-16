42fe2801ee8e82f4b37f2f174c72fbcaae9252f4
dni ⚡ <office@dnilabs.com>
2023-06-16 09:52:40 +0200

    remove console logs

--- src/utils/feeChecker.js
+++ src/utils/feeChecker.js
@@ -3,7 +3,5 @@ import { config, asset } from "../signals.js";
 export const feeChecker = (pairs) => {
     const oldHash = config()[`${asset()}/BTC`]["hash"];
     const hash = pairs[`${asset()}/BTC`]["hash"];
-    console.log(`oldHash: ${oldHash}`);
-    console.log(`hash: ${hash}`);
     return oldHash === hash;
 };
