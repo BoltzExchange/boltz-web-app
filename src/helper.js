9cb256716d3cc1aba8bb10d1362ea098e8d6e0df
michael1011 <me@michael1011.at>
2023-07-28 14:02:30 +0200

    refactor: WebLN enable callback

--- src/helper.js
+++ src/helper.js
@@ -179,46 +179,6 @@ export const qr = (data, cb) => {
         });
 };
 
-export async function detectWebLNProvider(timeoutParam) {
-    const timeout = timeoutParam ?? 3000;
-    const interval = 100;
-    let handled = false;
-
-    return new Promise((resolve) => {
-        if (window.webln) {
-            handleWebLN();
-        } else {
-            document.addEventListener("webln:ready", handleWebLN, {
-                once: true,
-            });
-
-            let i = 0;
-            const checkInterval = setInterval(function () {
-                if (window.webln || i >= timeout / interval) {
-                    handleWebLN();
-                    clearInterval(checkInterval);
-                }
-                i++;
-            }, interval);
-        }
-
-        function handleWebLN() {
-            if (handled) {
-                return false;
-            }
-            handled = true;
-
-            document.removeEventListener("webln:ready", handleWebLN);
-
-            if (window.webln) {
-                resolve(window.webln);
-            } else {
-                resolve(false);
-            }
-        }
-    });
-}
-
 export async function refund(swap, t) {
     let output = "";
     setRefundTx("");
