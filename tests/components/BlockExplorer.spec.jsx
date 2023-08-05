708eb3b9c98c89c3d299e1eb6fb0d4b5f2145e08
dni ⚡ <office@dnilabs.com>
2023-08-05 17:33:14 +0200

    fixup test? WTF?

--- tests/components/BlockExplorer.spec.jsx
+++ tests/components/BlockExplorer.spec.jsx
@@ -31,7 +31,7 @@ describe("BlockExplorer", () => {
     );
 
     test.each`
-        asset      | baseLink                    | address
+        asset      | baseLink                    | txId
         ${"BTC"}   | ${blockexplorer_url}        | ${"813c90372c9b774396c66099cf8015f9510a8ba5686cbb78d8e848959fe7bb5d"}
         ${"L-BTC"} | ${blockexplorer_url_liquid} | ${"9193b769c217808a17a86890195851eab78fdfd2f14d877163587327620324af"}
     `(
