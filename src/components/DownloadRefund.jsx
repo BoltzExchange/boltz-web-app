56a26dc0781bcfc40ad861d75ad6cfac82359d4a
michael1011 <me@michael1011.at>
2023-08-17 10:12:49 +0200

    fix: refund QR generation on mobile Tor browser

--- src/components/DownloadRefund.jsx
+++ src/components/DownloadRefund.jsx
@@ -1,9 +1,7 @@
-import log from "loglevel";
-import QRCode from "qrcode";
 import { useI18n } from "@solid-primitives/i18n";
-import { isIos, isMobile } from "../helper";
+import { swap } from "../signals";
+import { isIos, isMobile, qr } from "../helper";
 import { download, downloadJson } from "../utils/download";
-import { swap, setNotificationType, setNotification } from "../signals";
 
 const createRefundData = (swap) => {
     return {
@@ -28,29 +26,20 @@ const DownloadRefund = () => {
     const [t] = useI18n();
 
     const downloadRefundQr = (swap) => {
-        QRCode.toDataURL(JSON.stringify(createRefundData(swap)), {
-            width: 400,
-        })
-            .then(async (url) => {
-                if (isIos) {
-                    // Compatibility with third party iOS browsers
-                    const newTab = window.open();
-                    newTab.document.body.innerHTML = `
+        qr(JSON.stringify(createRefundData(swap)), (url) => {
+            if (isIos) {
+                // Compatibility with third party iOS browsers
+                const newTab = window.open();
+                newTab.document.body.innerHTML = `
                     <!DOCTYPE html>
                     <body>
                         <img src="${url}">
                         <h1>${t("ios_image_download")}</h1>
                     </body>`;
-                } else {
-                    download(`${getRefundFileName(swap)}.png`, url);
-                }
-            })
-            .catch((err) => {
-                const msg = "error: QR code generation failed";
-                log.error(msg, err);
-                setNotificationType("error");
-                setNotification(msg);
-            });
+            } else {
+                download(`${getRefundFileName(swap)}.png`, url);
+            }
+        });
     };
 
     return (
