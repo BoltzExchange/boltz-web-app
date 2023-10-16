2ca7ee653122308609bc5c82fa1a4e5d30ef1c75
dni ⚡ <office@dnilabs.com>
2023-10-16 09:54:57 +0200

    prettier

--- src/utils/feeChecker.js
+++ src/utils/feeChecker.js
@@ -4,14 +4,19 @@ export const feeChecker = (pairs) => {
     const oldCfg = config()[`${asset()}/BTC`];
     const minerFeesOld = oldCfg.fees.minerFees;
     const minerFees = pairs[`${asset()}/BTC`].fees.minerFees;
-    if (minerFeesOld.baseAsset.normal !== minerFees.baseAsset.normal
-        || minerFeesOld.baseAsset.reverse.claim !== minerFees.baseAsset.reverse.claim
-        || minerFeesOld.baseAsset.reverse.lockup !== minerFees.baseAsset.reverse.lockup
-        || minerFeesOld.quoteAsset.normal !== minerFees.quoteAsset.normal
-        || minerFeesOld.quoteAsset.reverse.claim !== minerFees.quoteAsset.reverse.claim
-        || minerFeesOld.quoteAsset.reverse.lockup !== minerFees.quoteAsset.reverse.lockup
+    if (
+        minerFeesOld.baseAsset.normal !== minerFees.baseAsset.normal ||
+        minerFeesOld.baseAsset.reverse.claim !==
+            minerFees.baseAsset.reverse.claim ||
+        minerFeesOld.baseAsset.reverse.lockup !==
+            minerFees.baseAsset.reverse.lockup ||
+        minerFeesOld.quoteAsset.normal !== minerFees.quoteAsset.normal ||
+        minerFeesOld.quoteAsset.reverse.claim !==
+            minerFees.quoteAsset.reverse.claim ||
+        minerFeesOld.quoteAsset.reverse.lockup !==
+            minerFees.quoteAsset.reverse.lockup
     ) {
-        return false
+        return false;
     }
     return true;
     // TODO: fix hashes in backend
--- tests/utils/feeChecker.spec.js
+++ tests/utils/feeChecker.spec.js
@@ -4,26 +4,26 @@ import { feeChecker } from "../../src/utils/feeChecker";
 const cfg = {
     "BTC/BTC": {
         hash: "3c45e394f2cb8b84ceee2a8e51849616a8981a7eab09e5194cd1ab7787b6b12e",
-        fees:{
-            percentage:0.5,
-            percentageSwapIn:0.1,
+        fees: {
+            percentage: 0.5,
+            percentageSwapIn: 0.1,
             minerFees: {
                 baseAsset: {
-                    normal:1870,
-                    reverse:{
-                        claim:1518,
-                        lockup:1683
-                    }
+                    normal: 1870,
+                    reverse: {
+                        claim: 1518,
+                        lockup: 1683,
+                    },
                 },
                 quoteAsset: {
-                    normal:1870,
+                    normal: 1870,
                     reverse: {
-                        claim:1518,
-                        lockup:1683
-                    }
-                }
-            }
-        }
+                        claim: 1518,
+                        lockup: 1683,
+                    },
+                },
+            },
+        },
     },
     "L-BTC/BTC": {
         hash: "8cc20e549082be62f4d043966c838694cfb11c7ac6aca7e62467b6911e1d5117",
@@ -33,26 +33,26 @@ const cfg = {
 const changed_cfg = {
     "BTC/BTC": {
         hash: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-        fees:{
-            percentage:0.5,
-            percentageSwapIn:0.1,
+        fees: {
+            percentage: 0.5,
+            percentageSwapIn: 0.1,
             minerFees: {
                 baseAsset: {
-                    normal:9999,
-                    reverse:{
-                        claim:1518,
-                        lockup:1683
-                    }
+                    normal: 9999,
+                    reverse: {
+                        claim: 1518,
+                        lockup: 1683,
+                    },
                 },
                 quoteAsset: {
-                    normal:1870,
+                    normal: 1870,
                     reverse: {
-                        claim:1518,
-                        lockup:1683
-                    }
-                }
-            }
-        }
+                        claim: 1518,
+                        lockup: 1683,
+                    },
+                },
+            },
+        },
     },
     "L-BTC/BTC": {
         hash: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
