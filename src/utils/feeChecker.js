5ab271f77ac7dc5b98b9e1206d9ed0994c012c56
dni ⚡ <office@dnilabs.com>
2023-10-16 09:46:35 +0200

    fix: check fee amount instead of pair hash
    closes #390
    closes #197

--- src/utils/feeChecker.js
+++ src/utils/feeChecker.js
@@ -1,7 +1,21 @@
 import { config, asset } from "../signals.js";
 
 export const feeChecker = (pairs) => {
-    const oldHash = config()[`${asset()}/BTC`]["hash"];
-    const hash = pairs[`${asset()}/BTC`]["hash"];
-    return oldHash === hash;
+    const oldCfg = config()[`${asset()}/BTC`];
+    const minerFeesOld = oldCfg.fees.minerFees;
+    const minerFees = pairs[`${asset()}/BTC`].fees.minerFees;
+    if (minerFeesOld.baseAsset.normal !== minerFees.baseAsset.normal
+        || minerFeesOld.baseAsset.reverse.claim !== minerFees.baseAsset.reverse.claim
+        || minerFeesOld.baseAsset.reverse.lockup !== minerFees.baseAsset.reverse.lockup
+        || minerFeesOld.quoteAsset.normal !== minerFees.quoteAsset.normal
+        || minerFeesOld.quoteAsset.reverse.claim !== minerFees.quoteAsset.reverse.claim
+        || minerFeesOld.quoteAsset.reverse.lockup !== minerFees.quoteAsset.reverse.lockup
+    ) {
+        return false
+    }
+    return true;
+    // TODO: fix hashes in backend
+    // const oldHash = config()[`${asset()}/BTC`]["hash"];
+    // const hash = pairs[`${asset()}/BTC`]["hash"];
+    // return oldHash === hash;
 };
