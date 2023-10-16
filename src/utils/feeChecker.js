10ee8496f12d59e0974c0b6c794e88b4c05ac624
michael1011 <me@michael1011.at>
2023-10-16 11:57:16 +0200

    fix: check only relevant fees

--- src/utils/feeChecker.js
+++ src/utils/feeChecker.js
@@ -1,15 +1,19 @@
+import { BTC } from "../consts.js";
 import { config, asset } from "../signals.js";
 
+const relevantMinerFees = (minerFees) => {
+    return asset() === BTC ? minerFees.quoteAsset : minerFees.baseAsset;
+};
+
 export const feeChecker = (pairs) => {
-    const oldCfg = config()[`${asset()}/BTC`];
-    const minerFeesOld = oldCfg.fees.minerFees;
-    const minerFees = pairs[`${asset()}/BTC`].fees.minerFees;
-    if (JSON.stringify(minerFeesOld) !== JSON.stringify(minerFees)) {
-        return false;
-    }
-    return true;
-    // TODO: fix hashes in backend
-    // const oldHash = config()[`${asset()}/BTC`]["hash"];
-    // const hash = pairs[`${asset()}/BTC`]["hash"];
-    // return oldHash === hash;
+    const fees = pairs[`${asset()}/${BTC}`].fees;
+    const feesOld = config()[`${asset()}/${BTC}`].fees;
+
+    return (
+        JSON.stringify(relevantMinerFees(feesOld.minerFees)) ===
+            JSON.stringify(relevantMinerFees(fees.minerFees)) &&
+        ["percentage", "percentageSwapIn"].every(
+            (fee) => feesOld[fee] === fees[fee]
+        )
+    );
 };
