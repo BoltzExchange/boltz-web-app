2f69adeed0ba331cc92c62a932fff04c21951758
dni ⚡ <office@dnilabs.com>
2023-11-16 16:46:00 +0100

    refactor out config.common.js

---
+++ src/configs/config.common.js
@@ -0,0 +1,12 @@
+export const defaultLanguage = "en";
+export const isBeta = false;
+
+export const torUrl =
+    "http://boltzzzbnus4m7mta3cxmflnps4fp7dueu2tgurstbvrbt6xswzcocyd.onion/";
+export const discordUrl = "https://discord.gg/QBvZGcW";
+export const twitterUrl = "https://twitter.com/boltzhq";
+export const githubUrl = "https://github.com/BoltzExchange";
+export const docsUrl = "https://docs.boltz.exchange";
+export const blogUrl = "https://blog.boltz.exchange";
+export const nostrUrl =
+    "https://snort.social/p/npub1psm37hke2pmxzdzraqe3cjmqs28dv77da74pdx8mtn5a0vegtlas9q8970";
--- src/configs/config.mainnet.js
+++ src/configs/config.mainnet.js
@@ -1,3 +1,5 @@
+export * from "./configs/config.common";
+
 const isTor = window?.location?.hostname.endsWith(".onion");
 
 const apiUrl = isTor
@@ -10,19 +12,7 @@ const blockExplorerUrlLiquid = isTor
     ? "http://liquidmom47f6s3m53ebfxn47p76a6tlnxib3wp6deux7wuzotdr6cyd.onion"
     : "https://liquid.network";
 
-export const torUrl =
-    "http://boltzzzbnus4m7mta3cxmflnps4fp7dueu2tgurstbvrbt6xswzcocyd.onion/";
-export const discordUrl = "https://discord.gg/QBvZGcW";
-export const twitterUrl = "https://twitter.com/boltzhq";
-export const githubUrl = "https://github.com/BoltzExchange";
-export const docsUrl = "https://docs.boltz.exchange";
-export const blogUrl = "https://blog.boltz.exchange";
-export const nostrUrl =
-    "https://snort.social/p/npub1psm37hke2pmxzdzraqe3cjmqs28dv77da74pdx8mtn5a0vegtlas9q8970";
-
-export const defaultLanguage = "en";
 export const network = "main";
-export const isBeta = false;
 export const bolt11_prefix = "lnbc";
 export const loglevel = "info";
 
