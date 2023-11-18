5136010594b52ec8eddedef52fadb7c8b9c4925a
dni ⚡ <office@dnilabs.com>
2023-11-18 10:59:56 +0100

    bug: out of context execution for effects/memo
    also gets rid of annoying erros when running `vitest`

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,12 +1,14 @@
 import { flatten, resolveTemplate, translator } from "@solid-primitives/i18n";
-import { createMemo } from "solid-js";
+import { createMemo, createRoot } from "solid-js";
 
 import { defaultLanguage } from "../config";
 import { i18n, i18nConfigured, setI18n } from "../signals";
 import dict from "./i18n";
 
-createMemo(() => setI18n(i18nConfigured()));
-
-const dictLocale = createMemo(() => flatten(dict[i18n() || defaultLanguage]));
+let dictLocale;
+createRoot(() => {
+    createMemo(() => setI18n(i18nConfigured()));
+    dictLocale = createMemo(() => flatten(dict[i18n() || defaultLanguage]));
+});
 
 export default translator(dictLocale, resolveTemplate);
