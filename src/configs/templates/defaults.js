7ca72df7f71f3cbb92c8af6fbd33831fb6f121f1
dni ⚡ <office@dnilabs.com>
2023-11-19 12:51:33 +0100

    refactor: move signals into components (#324)
    * chore: clean up, move signals into components
    
    use ambossUrl, cleanup Hero
    
    * fixup!

--- src/configs/templates/defaults.js
+++ src/configs/templates/defaults.js
@@ -2,6 +2,8 @@ export const defaultLanguage = "en";
 export const isBeta = false;
 
 export const torUrl = "";
+export const ambossUrl =
+    "https://amboss.space/node/026165850492521f4ac8abd9bd8088123446d126f648ca35e60f88177dc149ceb2";
 export const discordUrl = "https://discord.gg/QBvZGcW";
 export const twitterUrl = "https://twitter.com/boltzhq";
 export const githubUrl = "https://github.com/BoltzExchange";
