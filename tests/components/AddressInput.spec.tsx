97db9745ac82099272c0e5640d4faac7ceaa0fb3
dni ⚡ <office@dnilabs.com>
2023-12-02 16:28:40 +0100

    "fix" addressinput

--- tests/components/AddressInput.spec.tsx
+++ tests/components/AddressInput.spec.tsx
@@ -8,22 +8,22 @@ import * as signals from "../../src/signals";
 
 describe("AddressInput", () => {
     test.each`
-        network | address                                                                                                    | valid
-        ${BTC}  | ${"mv5v8C3e1SySwqe6r2fq9Fh6DbZr8ddjsX"}                                                                    | ${true}
-        ${BTC}  | ${"2N17VNGbi4yUHtkD7vhrc8cpi9JGVmC8scn"}                                                                   | ${true}
-        ${BTC}  | ${"bcrt1q7vq47xpsg4t080205edaulc3sdsjpdxy9svhr3"}                                                          | ${true}
-        ${BTC}  | ${"bcrt1pjyk4csn4nd4apwqy8s2p5kj5kywtrwzejtjalz3sufeljsycxw3qgrstgd"}                                      | ${true}
-        ${BTC}  | ${"02d96eadea3d780104449aca5c93461ce67c1564e2e1d73225fa67dd3b997a6018"}                                    | ${false}
-        ${LBTC} | ${"bcrt1pjyk4csn4nd4apwqy8s2p5kj5kywtrwzejtjalz3sufeljsycxw3qgrstgd"}                                      | ${false}
-        ${LBTC} | ${"CTEyTteD4cQg2NfF1yGWUU1rWSDC8sKHrj5BZJzr8kzyKFXwNCJ8VyDhi45Q98KdSf3jeTkbjJy18JkP"}                      | ${true}
-        ${LBTC} | ${"AzpjfmC41JpC6ieu3odwFBqtF4isFeY8RHv1e699EM2RgiyHd49og66a8qLLMDrhL8pCLeWAxJat1ebD"}                      | ${true}
-        ${LBTC} | ${"el1qqt7nl8pw6278yxv38fezzw8lmqh40prpusfurcvsh2xn3sl0pvnz3whllcapdnxcxn2u0wumpu7u2u6anh2juvmz7spx6snmn"} | ${true}
-        ${LBTC} | ${"2do9j7MdMJSVKWosUmcLzhoR8E5mhabtUju"}                                                                   | ${true}
-        ${LBTC} | ${"XUWfSHgUE1G72X9oGHXfecgzgf1N5A7WD2"}                                                                    | ${true}
-        ${LBTC} | ${"ert1qhtlluwskenvrf4w8hwds70w9wdwem4fwsd0pk8"}                                                           | ${true}
+        valid    | network | address
+        ${true}  | ${BTC}  | ${"mv5v8C3e1SySwqe6r2fq9Fh6DbZr8ddjsX"}
+        ${true}  | ${BTC}  | ${"2N17VNGbi4yUHtkD7vhrc8cpi9JGVmC8scn"}
+        ${true}  | ${BTC}  | ${"bcrt1q7vq47xpsg4t080205edaulc3sdsjpdxy9svhr3"}
+        ${true}  | ${BTC}  | ${"bcrt1pjyk4csn4nd4apwqy8s2p5kj5kywtrwzejtjalz3sufeljsycxw3qgrstgd"}
+        ${false} | ${BTC}  | ${"02d96eadea3d780104449aca5c93461ce67c1564e2e1d73225fa67dd3b997a6018"}
+        ${false} | ${LBTC} | ${"bcrt1pjyk4csn4nd4apwqy8s2p5kj5kywtrwzejtjalz3sufeljsycxw3qgrstgd"}
+        ${true}  | ${LBTC} | ${"CTEyTteD4cQg2NfF1yGWUU1rWSDC8sKHrj5BZJzr8kzyKFXwNCJ8VyDhi45Q98KdSf3jeTkbjJy18JkP"}
+        ${true}  | ${LBTC} | ${"AzpjfmC41JpC6ieu3odwFBqtF4isFeY8RHv1e699EM2RgiyHd49og66a8qLLMDrhL8pCLeWAxJat1ebD"}
+        ${true}  | ${LBTC} | ${"el1qqt7nl8pw6278yxv38fezzw8lmqh40prpusfurcvsh2xn3sl0pvnz3whllcapdnxcxn2u0wumpu7u2u6anh2juvmz7spx6snmn"}
+        ${true}  | ${LBTC} | ${"2do9j7MdMJSVKWosUmcLzhoR8E5mhabtUju"}
+        ${true}  | ${LBTC} | ${"XUWfSHgUE1G72X9oGHXfecgzgf1N5A7WD2"}
+        ${true}  | ${LBTC} | ${"ert1qhtlluwskenvrf4w8hwds70w9wdwem4fwsd0pk8"}
     `(
         "should validate address $network $address -> $valid",
-        async ({ network, address, valid }) => {
+        async ({ valid, network, address }) => {
             const setAddressValid = vi.spyOn(signals, "setAddressValid");
             const setOnchainAddress = vi.spyOn(signals, "setOnchainAddress");
 
@@ -39,7 +39,7 @@ describe("AddressInput", () => {
                 target: { value: address },
             });
 
-            expect(setAddressValid).toHaveBeenCalledTimes(1);
+            expect(setAddressValid).toHaveBeenCalledTimes(2);
             expect(setAddressValid).toHaveBeenCalledWith(valid);
 
             if (valid) {
