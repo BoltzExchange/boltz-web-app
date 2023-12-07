00aee2a5de6b0f34167b867191ad581547c5624c
dni ⚡ <office@dnilabs.com>
2023-12-07 09:16:26 +0100

    bug: lnurls and lnaddress can have a prefix aswell

--- src/utils/invoice.ts
+++ src/utils/invoice.ts
@@ -129,7 +129,7 @@ const emailRegex =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 export const isLnurl = (data: string) => {
-    data = data.toLowerCase();
+    data = trimLightningPrefix(data.toLowerCase());
     return (
         (data.includes("@") && emailRegex.test(data)) ||
         (data.startsWith("lnurl") && isValidBech32(data))
