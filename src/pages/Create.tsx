db9d0c8145b42199d423f9f4cac30f1f69af9441
dni ⚡ <office@dnilabs.com>
2024-01-06 23:09:05 +0100

    fixup!

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -2,18 +2,17 @@ import { BigNumber } from "bignumber.js";
 import log from "loglevel";
 import { Show, createEffect, createMemo, on, onMount } from "solid-js";
 
-import AddressInput from "./components/AddressInput";
-import Asset from "./components/Asset";
-import AssetSelect from "./components/AssetSelect";
-import ConnectMetamask from "./components/ConnectMetamask";
-import { CreateButton, setButtonLabel } from "./components/CreateButton";
-import Fees from "./components/Fees";
-import InvoiceInput from "./components/InvoiceInput";
-import QrScan from "./components/QrScan";
-import Reverse from "./components/Reverse";
-import { RBTC, sideReceive, sideSend } from "./consts";
-import { isMobile } from "./helper";
-import t from "./i18n";
+import AddressInput from "../components/AddressInput";
+import Asset from "../components/Asset";
+import AssetSelect from "../components/AssetSelect";
+import ConnectMetamask from "../components/ConnectMetamask";
+import { CreateButton, setButtonLabel } from "../components/CreateButton";
+import Fees from "../components/Fees";
+import InvoiceInput from "../components/InvoiceInput";
+import QrScan from "../components/QrScan";
+import Reverse from "../components/Reverse";
+import { RBTC, sideReceive, sideSend } from "../consts";
+import t from "../i18n";
 import {
     addressValid,
     amountChanged,
