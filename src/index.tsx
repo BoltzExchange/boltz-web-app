9630fc8b8635a42643e1eef8720915783a7d8e03
jackstar12 <jkranawetter05@gmail.com>
2024-03-18 10:52:56 +0100

    feat: error handling

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import "@fontsource/noto-sans";
 import "@fontsource/noto-sans/800.css";
 import { Route, Router } from "@solidjs/router";
 import log from "loglevel";
+import { Match, Switch, createSignal, onMount } from "solid-js";
 import { Show, render } from "solid-js/web";
 
 import Footer from "./components/Footer";
@@ -33,60 +34,74 @@ if ("serviceWorker" in navigator) {
         });
 }
 
-document.body.classList.remove("loading");
-
 const isEmbedded = () => {
     return useGlobalContext().embedded();
 };
 
-// TODO: error handling
-fetch("/config.json").then(async (response) => {
-    const data = await response.json();
-    setConfig(data);
+const App = (props: any) => {
+    const [configError, setConfigError] = createSignal<boolean>(null);
 
-    const App = (props: any) => {
-        return (
-            <GlobalProvider>
-                <Web3SignerProvider>
-                    <CreateProvider>
-                        <PayProvider>
-                            <SwapChecker />
-                            <Show when={!isEmbedded()}>
-                                <Nav network={config.network} />
-                            </Show>
-                            {props.children}
-                            <Notification />
-                            <Show when={!isEmbedded()}>
-                                <Footer />
-                            </Show>
-                        </PayProvider>
-                    </CreateProvider>
-                </Web3SignerProvider>
-            </GlobalProvider>
-        );
-    };
+    onMount(async () => {
+        try {
+            const response = await fetch("/config.json");
+            const data = await response.json();
+            setConfig(data);
+            setConfigError(false);
+        } catch (error) {
+            setConfigError(true);
+            console.error("Error loading config:", error);
+        }
+        document.body.classList.remove("loading");
+    });
 
-    const cleanup = render(
-        () => (
-            <Router root={App}>
-                <Route path="/" component={Hero} />
-                <Route path="/swap" component={Create} />
-                {/* Compatibility with link in Breez:
-                                https://github.com/breez/breezmobile/blob/a1b0ffff902dfa2210af8fdb047b715535ff11e9/src/json/vendors.json#L30 */}
-                <Route path="/swapbox" component={Create} />
-                <Route path="/swap/:id" component={Pay} />
-                <Route path="/swap/refund/:id" component={RefundStep} />
-                <Route path="/error" component={Error} />
-                <Route path="/refund" component={Refund} />
-                <Route path="/history" component={History} />
-                <Route path="*404" component={NotFound} />
-            </Router>
-        ),
-        document.getElementById("root"),
+    return (
+        <Switch>
+            <Match when={configError() === true}>
+                <div>Invalid or missing app configuration</div>
+            </Match>
+            <Match when={configError() === false}>
+                <GlobalProvider>
+                    <Web3SignerProvider>
+                        <CreateProvider>
+                            <PayProvider>
+                                <SwapChecker />
+                                <Show when={!isEmbedded()}>
+                                    <Nav network={config.network} />
+                                </Show>
+                                {props.children}
+                                <Notification />
+                                <Show when={!isEmbedded()}>
+                                    <Footer />
+                                </Show>
+                            </PayProvider>
+                        </CreateProvider>
+                    </Web3SignerProvider>
+                </GlobalProvider>
+            </Match>
+        </Switch>
     );
+};
 
-    if (import.meta.hot) {
-        console.log("Hot reload");
-        import.meta.hot.dispose(cleanup);
-    }
-});
+const cleanup = render(
+    () => (
+        <Router root={App}>
+            <Route path="/" component={Hero} />
+            <Route path="/swap" component={Create} />
+            {/* Compatibility with link in Breez:
+                                https://github.com/breez/breezmobile/blob/a1b0ffff902dfa2210af8fdb047b715535ff11e9/src/json/vendors.json#L30 */}
+            <Route path="/swapbox" component={Create} />
+            <Route path="/swap/:id" component={Pay} />
+            <Route path="/swap/refund/:id" component={RefundStep} />
+            <Route path="/error" component={Error} />
+            <Route path="/refund" component={Refund} />
+            <Route path="/history" component={History} />
+            <Route path="*404" component={NotFound} />
+        </Router>
+    ),
+    document.getElementById("root"),
+);
+
+if (import.meta.hot) {
+    console.log("Hot reload");
+    import.meta.hot.dispose(cleanup);
+}
