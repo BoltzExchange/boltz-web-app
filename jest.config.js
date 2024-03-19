0ab05215853206bd1bf0a25b1f1aa99251d3a34e
michael1011 <me@michael1011.at>
2024-03-19 11:34:14 +0100

    fix: tests with icon font

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     preset: "solid-jest/preset/browser",
     setupFilesAfterEnv: ["<rootDir>/node_modules/@testing-library/jest-dom"],
-    transformIgnorePatterns: ["node_modules/(?!@solidjs)"],
+    transformIgnorePatterns: ["node_modules/(?!@solidjs|solid-icons)"],
     moduleNameMapper: {
         "^.+\\.svg": "<rootDir>/tests/mocks/SvgMock.tsx",
         "^.+\\.css": "<rootDir>/tests/mocks/StylesMock.tsx",
