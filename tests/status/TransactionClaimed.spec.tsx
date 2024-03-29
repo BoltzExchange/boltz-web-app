00c253c1bc6a1cae4960376e73d801dda77c8eb8
dni ⚡ <office@dnilabs.com>
2024-03-29 15:55:02 +0100

    fix: multiple claim transactions being broadcasted (#542)
    * fix: claim duplications
    
    the effect in TxClaimed was triggering the claim, i think this is not necessary because the sweapchecker handles it. tested it manually and it works.
    
    @michael1011 i think you introduced that any reasoning behind that?
    
    introduction of claim queue so websocket updates do not overlap each other
    
    * refator: queue of WebSocket messages to handle
    
    * fixup!
    
    * fix: handle rejected promises in lock
    
    * chore: fix wording of test
    
    ---------
    
    Co-authored-by: michael1011 <me@michael1011.at>

--- tests/status/TransactionClaimed.spec.tsx
+++ tests/status/TransactionClaimed.spec.tsx
@@ -3,28 +3,8 @@ import { render, screen } from "@solidjs/testing-library";
 import { BTC, LBTC, RBTC } from "../../src/consts";
 import i18n from "../../src/i18n/i18n";
 import TransactionClaimed from "../../src/status/TransactionClaimed";
-import { getReverseTransaction } from "../../src/utils/boltzClient";
-import { claim } from "../../src/utils/claim";
-import {
-    TestComponent,
-    contextWrapper,
-    globalSignals,
-    payContext,
-} from "../helper";
+import { TestComponent, contextWrapper, payContext } from "../helper";
 
-let claimPromiseResolve: (() => void) | undefined = undefined;
-
-jest.mock("../../src/utils/claim", () => ({
-    claim: jest.fn().mockImplementation(
-        async (swap) =>
-            new Promise<void>((resolve) => {
-                claimPromiseResolve = () => {
-                    swap.claimTx = "claimedTxId";
-                    resolve(swap);
-                };
-            }),
-    ),
-}));
 jest.mock("../../src/utils/boltzClient", () => ({
     getReverseTransaction: jest.fn().mockResolvedValue({
         hex: "txHex",
@@ -63,54 +43,4 @@ describe("TransactionClaimed", () => {
             screen.findByText(i18n.en.congrats),
         ).resolves.not.toBeUndefined();
     });
-
-    test.each`
-        symbol
-        ${BTC}
-        ${LBTC}
-    `(
-        "should trigger claim for reverse swaps to $symbol with no claim transaction",
-        async ({ symbol }) => {
-            render(
-                () => (
-                    <>
-                        <TestComponent />
-                        <TransactionClaimed />
-                    </>
-                ),
-                {
-                    wrapper: contextWrapper,
-                },
-            );
-
-            const swap = {
-                id: "swapId",
-                asset: symbol,
-                reverse: true,
-            };
-
-            globalSignals.setSwaps([JSON.parse(JSON.stringify(swap))]);
-            payContext.setSwap(swap);
-
-            await expect(
-                screen.findByText(i18n.en.broadcasting_claim),
-            ).resolves.not.toBeUndefined();
-
-            claimPromiseResolve();
-
-            await expect(
-                screen.findByText(i18n.en.congrats),
-            ).resolves.not.toBeUndefined();
-
-            expect(getReverseTransaction).toHaveBeenCalledTimes(1);
-            expect(getReverseTransaction).toHaveBeenCalledWith(
-                swap.asset,
-                swap.id,
-            );
-            expect(claim).toHaveBeenCalledTimes(1);
-
-            expect(payContext.swap().claimTx).toEqual("claimedTxId");
-            expect(globalSignals.swaps()[0].claimTx).toEqual("claimedTxId");
-        },
-    );
 });
