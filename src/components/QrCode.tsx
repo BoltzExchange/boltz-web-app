f730c03b9d9637142e13f60415a5c900be90f197
dni ⚡ <office@dnilabs.com>
2024-04-22 16:40:46 +0200

    feat: show qr code icon depending on invoice/onchain asset
    closes #558

--- src/components/QrCode.tsx
+++ src/components/QrCode.tsx
@@ -6,6 +6,7 @@ import "../style/qrcode.scss";
 
 interface QrCodeProps {
     data: string;
+    asset?: string;
 }
 
 export const Qrcode = (params: QrCodeProps) => {
@@ -18,7 +19,7 @@ export const Qrcode = (params: QrCodeProps) => {
         });
 
     return (
-        <div id="qrcode" style="position: relative;">
+        <div data-asset={params.asset} id="qrcode" style="position: relative;">
             <img src={dataUrl()} alt="Payment QR Code" />
             <span></span>
         </div>
