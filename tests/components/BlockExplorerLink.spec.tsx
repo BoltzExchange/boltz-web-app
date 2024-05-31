154e1085cb15e228a0a32674ed32b7d8015b07e1
michael1011 <me@michael1011.at>
2024-05-31 20:52:20 +0200

    refactor: move block explorer link out of Pay page (#613)

---
+++ tests/components/BlockExplorerLink.spec.tsx
@@ -0,0 +1,151 @@
+import { render, screen } from "@solidjs/testing-library";
+import { createSignal } from "solid-js";
+
+import BlockExplorerLink, {
+    TransactionType,
+} from "../../src/components/BlockExplorerLink";
+import { config } from "../../src/config";
+import { BTC, LBTC } from "../../src/consts/Assets";
+import { SwapType } from "../../src/consts/Enums";
+import { ChainSwap, SomeSwap } from "../../src/utils/swapCreator";
+import { contextWrapper } from "../helper";
+
+describe("BlockExplorerLink", () => {
+    describe("Submarine and Reverse Swaps", () => {
+        test.each`
+            type                  | asset   | address  | params
+            ${SwapType.Submarine} | ${BTC}  | ${"bc1"} | ${{ type: SwapType.Submarine, assetSend: BTC, address: "bc1" }}
+            ${SwapType.Reverse}   | ${LBTC} | ${"lq1"} | ${{ type: SwapType.Reverse, assetReceive: LBTC, lockupAddress: "lq1" }}
+        `(
+            "should show lockup address for $type when not claimed yet",
+            async ({ params, asset, address }) => {
+                const [swap] = createSignal<SomeSwap>(params);
+
+                render(
+                    () => (
+                        <BlockExplorerLink
+                            swap={swap}
+                            swapStatus={() => "transaction.mempool"}
+                            contractTransaction={() => undefined}
+                            contractTransactionType={() =>
+                                TransactionType.Lockup
+                            }
+                        />
+                    ),
+                    { wrapper: contextWrapper },
+                );
+
+                const button = (await screen.findByText(
+                    "open lockup address",
+                )) as HTMLAnchorElement;
+
+                expect(button.href).toEqual(
+                    `${config.assets[asset].blockExplorerUrl.normal}/address/${address}`,
+                );
+            },
+        );
+
+        test.each`
+            type                  | asset   | params
+            ${SwapType.Submarine} | ${BTC}  | ${{ type: SwapType.Submarine, assetSend: BTC, claimTx: "123" }}
+            ${SwapType.Reverse}   | ${LBTC} | ${{ type: SwapType.Reverse, assetReceive: LBTC, claimTx: "123" }}
+        `(
+            "should show claim transaction for $type",
+            async ({ params, asset }) => {
+                const [swap] = createSignal<SomeSwap>(params);
+
+                render(
+                    () => (
+                        <BlockExplorerLink
+                            swap={swap}
+                            swapStatus={() => "transaction.claimed"}
+                            contractTransaction={() => undefined}
+                            contractTransactionType={() =>
+                                TransactionType.Lockup
+                            }
+                        />
+                    ),
+                    { wrapper: contextWrapper },
+                );
+
+                const button = (await screen.findByText(
+                    "open claim transaction",
+                )) as HTMLAnchorElement;
+
+                expect(button.href).toEqual(
+                    `${config.assets[asset].blockExplorerUrl.normal}/tx/${params.claimTx}`,
+                );
+            },
+        );
+    });
+
+    describe("Chain Swaps", () => {
+        test("should show lockup address when not claimed yet", async () => {
+            const [swap] = createSignal<ChainSwap>({
+                type: SwapType.Chain,
+                assetSend: LBTC,
+                assetReceive: BTC,
+                lockupDetails: {
+                    lockupAddress: "bc1",
+                },
+            } as any);
+
+            render(
+                () => (
+                    <BlockExplorerLink
+                        swap={swap}
+                        swapStatus={() => ""}
+                        contractTransaction={() => undefined}
+                        contractTransactionType={() => TransactionType.Lockup}
+                    />
+                ),
+                { wrapper: contextWrapper },
+            );
+
+            const button = (await screen.findByText(
+                "open lockup address",
+            )) as HTMLAnchorElement;
+
+            expect(button.href).toEqual(
+                `${config.assets[LBTC].blockExplorerUrl.normal}/address/${swap().lockupDetails.lockupAddress}`,
+            );
+        });
+
+        test("should reactively show claim transaction", async () => {
+            const [swap, setSwap] = createSignal<ChainSwap>({
+                type: SwapType.Chain,
+                assetSend: LBTC,
+                assetReceive: BTC,
+                lockupDetails: {
+                    lockupAddress: "bc1",
+                },
+            } as any);
+
+            render(
+                () => (
+                    <BlockExplorerLink
+                        swap={swap}
+                        swapStatus={() => ""}
+                        contractTransaction={() => undefined}
+                        contractTransactionType={() => TransactionType.Lockup}
+                    />
+                ),
+                { wrapper: contextWrapper },
+            );
+
+            const button = (await screen.findByText(
+                "open lockup address",
+            )) as HTMLAnchorElement;
+
+            expect(button.href).toEqual(
+                `${config.assets[LBTC].blockExplorerUrl.normal}/address/${swap().lockupDetails.lockupAddress}`,
+            );
+
+            setSwap({ ...swap(), claimTx: "123" });
+
+            expect(button.href).toEqual(
+                `${config.assets[BTC].blockExplorerUrl.normal}/tx/${swap().claimTx}`,
+            );
+        });
+    });
+});
