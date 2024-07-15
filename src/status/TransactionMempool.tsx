6c32f0a1c324aac2434749ecc2326641cd446cf5
dni ⚡ <office@dnilabs.com>
2024-07-15 15:50:43 +0200

    add blockexplorerlink to mempool and success

--- src/status/TransactionMempool.tsx
+++ src/status/TransactionMempool.tsx
@@ -1,13 +1,21 @@
+import BlockExplorer from "../components/BlockExplorer";
 import LoadingSpinner from "../components/LoadingSpinner";
 import { useGlobalContext } from "../context/Global";
+import { usePayContext } from "../context/Pay";
 
 const TransactionMempool = () => {
     const { t } = useGlobalContext();
+    const { swap, swapStatusTransaction } = usePayContext();
     return (
         <div>
             <h2>{t("tx_in_mempool")}</h2>
             <p>{t("tx_in_mempool_subline")}</p>
             <LoadingSpinner />
+            <BlockExplorer
+                asset={swap().assetSend}
+                txId={swapStatusTransaction()?.id}
+                typeLabel="lockup_tx"
+            />
         </div>
     );
 };
