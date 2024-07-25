9a45b7e43bd16641a869053f8191c9ddb57d3697
michael1011 <me@michael1011.at>
2024-07-25 11:17:39 +0200

    fix: allow single refund file to be uploaded as backup (#651)

---
+++ tests/pages/History.spec.tsx
@@ -0,0 +1,50 @@
+import { SwapType } from "../../src/consts/Enums";
+import { Errors, validateBackupFile } from "../../src/pages/History";
+import { latestStorageVersion } from "../../src/utils/migration";
+
+describe("History", () => {
+    test.each`
+        data
+        ${[]}
+        ${[{ id: "1" }]}
+        ${[{ id: "1" }, { id: "2" }]}
+    `("should validate legacy backup files", ({ data }) => {
+        expect(validateBackupFile(data)).toEqual({
+            version: 0,
+            swaps: data,
+        });
+    });
+
+    test.each`
+        data
+        ${{ version: 1, swaps: [] }}
+        ${{ version: 1, swaps: [{ id: "taproot" }] }}
+    `("should validate new backup files", ({ data }) => {
+        expect(validateBackupFile(data)).toEqual(data);
+    });
+
+    test("should validate single swap refund files", () => {
+        const file = { id: "asdf", type: SwapType.Chain };
+        expect(validateBackupFile(file as any)).toEqual({
+            version: latestStorageVersion,
+            swaps: [file],
+        });
+    });
+
+    test.each`
+        error                            | data
+        ${Errors.InvalidBackupFile}      | ${{}}
+        ${Errors.InvalidBackupFile}      | ${"test"}
+        ${Errors.InvalidBackupFile}      | ${1}
+        ${Errors.InvalidBackupFile}      | ${{ version: 1 }}
+        ${Errors.InvalidBackupFile}      | ${{ swaps: [] }}
+        ${Errors.InvalidBackupFile}      | ${{ id: "asdf" }}
+        ${Errors.NotAllElementsHaveAnId} | ${{ version: 1, swaps: [{ id: null }] }}
+        ${Errors.NotAllElementsHaveAnId} | ${[{}]}
+        ${Errors.NotAllElementsHaveAnId} | ${[{ id: undefined }]}
+        ${Errors.NotAllElementsHaveAnId} | ${[{ id: null }]}
+        ${Errors.NotAllElementsHaveAnId} | ${[{ id: "1" }, { noId: "2" }]}
+    `("should throw for invalid backup file: $data", ({ data, error }) => {
+        expect(() => validateBackupFile(data)).toThrow(error);
+    });
+});
