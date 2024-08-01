ed130db59e2f8f36f87ae762274cc0ed34817bec
maybeast <78227110+maybeast@users.noreply.github.com>
2024-08-01 16:53:39 +0300

    works

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        baseURL: "https://localhost:4173",
+        baseURL: "https://localhost:5173",
         ignoreHTTPSErrors: true,
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
@@ -47,13 +47,11 @@ export default defineConfig({
     */
     ],
 
-    /* Run your local dev server before starting the tests */
     webServer: {
-        command: "npm run build && npm run preview -- --host 0.0.0.0",
-        url: 'https://127.0.0.1:4173',
-        // port: 4173,
-        reuseExistingServer: !process.env.CI,
-        stdout: "pipe",
-        stderr: "pipe",
+      command: 'npm run start',
+      port: 5173,
+      reuseExistingServer: !process.env.CI,
+      stdout: 'pipe',
+      stderr: 'pipe',
     },
 });
