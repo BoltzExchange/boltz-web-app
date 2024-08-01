db80758428b2d5c679cccf84bc5699dd7fe1e76f
michael1011 <me@michael1011.at>
2024-08-01 17:49:07 +0200

    move playwright scripts to package.json

--- playwright.config.ts
+++ playwright.config.ts
@@ -48,10 +48,10 @@ export default defineConfig({
     ],
 
     webServer: {
-      command: 'npm run start',
-      port: 5173,
-      reuseExistingServer: !process.env.CI,
-      stdout: 'pipe',
-      stderr: 'pipe',
+        command: "npm run start",
+        port: 5173,
+        reuseExistingServer: !process.env.CI,
+        stdout: "pipe",
+        stderr: "pipe",
     },
 });
