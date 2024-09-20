20e215ed889a3ee929c4d51d0f4743572e9256b3
SwapMarket <SwapMarket@proton.me>
2024-09-21 00:05:47 +0200

    Add base

--- vite.config.mjs
+++ vite.config.mjs
@@ -15,6 +15,7 @@ export default defineConfig({
         https: true,
         cors: { origin: "*" },
     },
+    base: '/',
     build: {
         commonjsOptions: {
             transformMixedEsModules: true,
