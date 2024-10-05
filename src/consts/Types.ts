9d9559897c3b8c0d096d47182104815f4887edf0
michael1011 <me@michael1011.at>
2024-10-05 16:43:50 +0200

    feat: trezor support

--- src/consts/Types.ts
+++ src/consts/Types.ts
@@ -4,3 +4,36 @@ export type ButtonLabelParams = {
     key: DictKey;
     params?: Record<string, string>;
 };
+
+export type EIP6963ProviderInfo = {
+    rdns: string;
+    uuid: string;
+    name: string;
+    icon?: string;
+    disabled?: boolean;
+};
+
+export type EIP1193Provider = {
+    isStatus?: boolean;
+    host?: string;
+    path?: string;
+    sendAsync?: (
+        request: { method: string; params?: Array<unknown> },
+        callback: (error: Error | null, response: unknown) => void,
+    ) => void;
+    send?: (
+        request: { method: string; params?: Array<unknown> },
+        callback: (error: Error | null, response: unknown) => void,
+    ) => void;
+    request: (request: {
+        method: string;
+        params?: Array<unknown>;
+    }) => Promise<unknown>;
+    on: (event: "chainChanged", cb: () => void) => void;
+    removeAllListeners: (event: "chainChanged") => void;
+};
+
+export type EIP6963ProviderDetail = {
+    info: EIP6963ProviderInfo;
+    provider: EIP1193Provider;
+};
