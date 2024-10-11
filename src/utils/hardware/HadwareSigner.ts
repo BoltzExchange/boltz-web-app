dff9966af8780d5449fefb37afeb180750637ac7
michael1011 <me@michael1011.at>
2024-10-11 18:27:44 +0200

    chore: allow RSK Test ledger app

--- src/utils/hardware/HadwareSigner.ts
+++ src/utils/hardware/HadwareSigner.ts
@@ -3,8 +3,12 @@ const derivationPaths = {
     Ethereum: "44'/60'/0'/0/0",
 };
 
+const derivationPathsTestnet = {
+    ["RSK Testnet"]: "44'/37310'/0'/0/0",
+};
+
 interface HardwareSigner {
     setDerivationPath(path: string): void;
 }
 
-export { HardwareSigner, derivationPaths };
+export { HardwareSigner, derivationPaths, derivationPathsTestnet };
