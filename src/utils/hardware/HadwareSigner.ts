ef8feeccc1d8b446678e26b16be2e9fd18e95722
michael1011 <me@michael1011.at>
2024-10-21 13:22:07 +0200

    fix: do not show testnet derivation path on mainnet

--- src/utils/hardware/HadwareSigner.ts
+++ src/utils/hardware/HadwareSigner.ts
@@ -1,14 +1,15 @@
-const derivationPaths = {
-    RSK: "44'/137'/0'/0/0",
+export const derivationPaths = {
     Ethereum: "44'/60'/0'/0/0",
 };
 
-const derivationPathsTestnet = {
+export const derivationPathsMainnet = {
+    RSK: "44'/137'/0'/0/0",
+};
+
+export const derivationPathsTestnet = {
     ["RSK Testnet"]: "44'/37310'/0'/0/0",
 };
 
-interface HardwareSigner {
+export interface HardwareSigner {
     setDerivationPath(path: string): void;
 }
-
-export { HardwareSigner, derivationPaths, derivationPathsTestnet };
