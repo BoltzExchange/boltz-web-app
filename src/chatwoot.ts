893a3e5a5b51cc8849afa21af379796669f3c5b8
michael1011 <me@michael1011.at>
2024-11-28 08:39:44 +0100

    chore: check chatwoot env variable in build script

--- src/chatwoot.ts
+++ src/chatwoot.ts
@@ -17,31 +17,35 @@ declare global {
     }
 }
 
-export default function Chatwoot() {
+const Chatwoot = () => {
     onMount(() => {
         const token = import.meta.env.VITE_CHATWOOT_TOKEN;
         const url = config.chatwootUrl;
-        if (token !== undefined && url !== undefined) {
-            const tag = "script";
-            const script = document.createElement(tag);
-            const parent = document.getElementsByTagName(tag)[0];
-            script.src = url + "/packs/js/sdk.js";
-            script.defer = true;
-            script.async = true;
-            parent.parentNode.insertBefore(script, parent);
-            script.onload = function () {
-                window.chatwootSettings = {
-                    darkMode: "auto",
-                };
-                window.chatwootSDK.run({
-                    websiteToken: token,
-                    baseUrl: url,
-                });
-            };
-        } else {
-            log.warn("Chatwoot token or url not set");
+
+        if (token === undefined || url === undefined) {
+            log.warn("Chatwoot URL or token not set");
+            return;
         }
+
+        const tag = "script";
+        const script = document.createElement(tag);
+        const parent = document.getElementsByTagName(tag)[0];
+        script.src = url + "/packs/js/sdk.js";
+        script.defer = true;
+        script.async = true;
+        parent.parentNode.insertBefore(script, parent);
+        script.onload = function () {
+            window.chatwootSettings = {
+                darkMode: "auto",
+            };
+            window.chatwootSDK.run({
+                websiteToken: token,
+                baseUrl: url,
+            });
+        };
     });
 
     return "";
-}
+};
+
+export default Chatwoot;
