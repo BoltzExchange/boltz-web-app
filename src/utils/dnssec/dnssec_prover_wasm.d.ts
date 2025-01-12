5be91f009469420f5dae2e0b4068fc870e00fd55
michael1011 <me@michael1011.at>
2025-01-12 16:12:03 +0100

    chore: bump dnssec-prover to v0.6.6 (#798)

--- src/utils/dnssec/dnssec_prover_wasm.d.ts
+++ src/utils/dnssec/dnssec_prover_wasm.d.ts
@@ -5,9 +5,6 @@
  * `name`.
  *
  * After calling this [`get_next_query`] should be called to fetch the initial query.
- * @param {string} name
- * @param {number} ty
- * @returns {WASMProofBuilder | undefined}
  */
 export function init_proof_builder(name: string, ty: number): WASMProofBuilder | undefined;
 /**
@@ -15,34 +12,26 @@ export function init_proof_builder(name: string, ty: number): WASMProofBuilder |
  *
  * After calling this, [`get_next_query`] should be called until pending queries are exhausted and
  * no more pending queries exist, at which point [`get_unverified_proof`] should be called.
- * @param {WASMProofBuilder} proof_builder
- * @param {Uint8Array} response
  */
 export function process_query_response(proof_builder: WASMProofBuilder, response: Uint8Array): void;
 /**
  * Gets the next query (if any) that should be sent to the resolver for the given proof builder.
  *
  * Once the resolver responds [`process_query_response`] should be called with the response.
- * @param {WASMProofBuilder} proof_builder
- * @returns {Uint8Array | undefined}
  */
 export function get_next_query(proof_builder: WASMProofBuilder): Uint8Array | undefined;
 /**
  * Gets the final, unverified, proof once all queries fetched via [`get_next_query`] have
  * completed and their responses passed to [`process_query_response`].
- * @param {WASMProofBuilder} proof_builder
- * @returns {Uint8Array}
  */
 export function get_unverified_proof(proof_builder: WASMProofBuilder): Uint8Array;
 /**
  * Verifies an RFC 9102-formatted proof and returns verified records matching the given name
  * (resolving any C/DNAMEs as required).
- * @param {Uint8Array} stream
- * @param {string} name_to_resolve
- * @returns {string}
  */
 export function verify_byte_stream(stream: Uint8Array, name_to_resolve: string): string;
 export class WASMProofBuilder {
+  private constructor();
   free(): void;
 }
 
@@ -53,9 +42,9 @@ export interface InitOutput {
   readonly __wbg_wasmproofbuilder_free: (a: number, b: number) => void;
   readonly init_proof_builder: (a: number, b: number, c: number) => number;
   readonly process_query_response: (a: number, b: number, c: number) => void;
-  readonly get_next_query: (a: number) => Array;
-  readonly get_unverified_proof: (a: number) => Array;
-  readonly verify_byte_stream: (a: number, b: number, c: number, d: number) => Array;
+  readonly get_next_query: (a: number) => [number, number];
+  readonly get_unverified_proof: (a: number) => [number, number, number, number];
+  readonly verify_byte_stream: (a: number, b: number, c: number, d: number) => [number, number];
   readonly __wbindgen_export_0: WebAssembly.Table;
   readonly __wbindgen_malloc: (a: number, b: number) => number;
   readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
