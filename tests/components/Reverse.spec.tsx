acd84d98323391f7daa99a2c8749631c747ce9f5
Caio Costa <costa.caiof@gmail.com>
2025-01-14 12:40:14 +0000

    test: check onchain address is cleared on direction switch

--- tests/components/Reverse.spec.tsx
+++ tests/components/Reverse.spec.tsx
@@ -1,7 +1,7 @@
 import { fireEvent, render } from "@solidjs/testing-library";
 
 import Reverse from "../../src/components/Reverse";
-import { BTC, LN } from "../../src/consts/Assets";
+import { BTC, LBTC, LN } from "../../src/consts/Assets";
 import { TestComponent, contextWrapper, signals } from "../helper";
 
 describe("Reverse", () => {
@@ -27,5 +27,29 @@ describe("Reverse", () => {
 
         expect(signals.assetSend()).toEqual(LN);
         expect(signals.assetReceive()).toEqual(BTC);
+    });
+
+    test("should clear onChainAddress on reverse", () => {
+        const {
+            container: { firstChild: flip },
+        } = render(
+            () => (
+                <>
+                    <Reverse />
+                    <TestComponent />
+                </>
+            ),
+            {
+                wrapper: contextWrapper,
+            },
+        );
+
+        signals.setOnchainAddress("2N17VNGbi4yUHtkD7vhrc8cpi9JGVmC8scn");
+        signals.setAssetSend(LBTC);
+        signals.setAssetReceive(BTC);
+
+        fireEvent.click(flip);
+
+        expect(signals.onchainAddress()).toEqual("");
     });
 });
