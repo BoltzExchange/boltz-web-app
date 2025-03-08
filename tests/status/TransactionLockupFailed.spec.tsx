7162c37963ccdc90b449f228d2bf102815d2e33e
Caio Costa <costa.caiof@gmail.com>
2025-03-07 23:55:42 -0300

    test: Check `RefundButton` is shown according to the existence of UTXOs

--- tests/status/TransactionLockupFailed.spec.tsx
+++ tests/status/TransactionLockupFailed.spec.tsx
@@ -3,8 +3,15 @@ import { OutputType } from "boltz-core";
 import { createSignal } from "solid-js";
 
 import { BTC, RBTC } from "../../src/consts/Assets";
+import { swapStatusFailed } from "../../src/consts/SwapStatus";
 import i18n from "../../src/i18n/i18n";
 import TransactionLockupFailed from "../../src/status/TransactionLockupFailed";
+import {
+    UTXO,
+    fetchRawTxWithFailover,
+    fetchUTXOsWithFailover,
+} from "../../src/utils/blockchain";
+import { getLockupTransaction } from "../../src/utils/boltzClient";
 import { SomeSwap } from "../../src/utils/swapCreator";
 import { TestComponent, contextWrapper, payContext } from "../helper";
 
@@ -13,6 +20,13 @@ vi.mock("../../src/utils/boltzClient", () => {
         getLockupTransaction: vi.fn(() => {
             return { timeoutBlockHeight: 10, timeoutEta: 10 };
         }),
+    };
+});
+
+vi.mock("../../src/utils/blockchain", () => {
+    return {
+        fetchUTXOsWithFailover: vi.fn(),
+        fetchRawTxWithFailover: vi.fn(),
     };
 });
 
@@ -40,13 +54,18 @@ describe("TransactionLockupFailed", () => {
                     wrapper: contextWrapper,
                 },
             );
+            payContext.setFailureReason(
+                "You will be able to refund after the timeout",
+            );
             payContext.setSwap({
                 assetReceive: BTC,
                 version: type,
             } as SomeSwap);
 
             await expect(
-                screen.findByText(i18n.en.refund_explainer),
+                screen.findByText((content) =>
+                    content.includes(i18n.en.refund_explainer),
+                ),
             ).resolves.not.toBeUndefined();
         },
     );
@@ -99,6 +118,90 @@ describe("TransactionLockupFailed", () => {
 
         await expect(
             screen.findByText(i18n.en.refund),
+        ).resolves.not.toBeUndefined();
+    });
+
+    test.each(Object.values(swapStatusFailed))(
+        "should show refund button for failed swap with any UTXO",
+        async (status) => {
+            vi.mocked(getLockupTransaction).mockImplementation(() => {
+                throw new Error();
+            });
+
+            // Sometimes users pay after the swap is completed (successfully or not),
+            // so we double-check for UTXOs against block explorers
+            vi.mocked(fetchUTXOsWithFailover).mockImplementation(() => {
+                return Promise.resolve([
+                    { txid: "0x", vout: 1, value: 100000000 },
+                ] as unknown as UTXO[]);
+            });
+            vi.mocked(fetchRawTxWithFailover).mockImplementation(() => {
+                return Promise.resolve({ hex: "0x" });
+            });
+
+            // eslint-disable-next-line solid/reactivity
+            const [, setStatusOverride] = createSignal<string>();
+
+            render(
+                () => (
+                    <>
+                        <TestComponent />
+                        <TransactionLockupFailed
+                            setStatusOverride={setStatusOverride}
+                        />
+                    </>
+                ),
+                {
+                    wrapper: contextWrapper,
+                },
+            );
+            payContext.setSwap({
+                assetReceive: BTC,
+                version: OutputType.Taproot,
+                status: status,
+            } as SomeSwap);
+
+            await expect(
+                screen.findByText(i18n.en.refund),
+            ).resolves.not.toBeUndefined();
+        },
+    );
+
+    test("should not show refund button for swap with no UTXO", async () => {
+        vi.mocked(getLockupTransaction).mockImplementation(() => {
+            throw new Error();
+        });
+        vi.mocked(fetchUTXOsWithFailover).mockImplementation(() => {
+            return Promise.resolve([] as unknown as UTXO[]);
+        });
+        vi.mocked(fetchRawTxWithFailover).mockImplementation(() => {
+            return null;
+        });
+
+        // eslint-disable-next-line solid/reactivity
+        const [, setStatusOverride] = createSignal<string>();
+
+        render(
+            () => (
+                <>
+                    <TestComponent />
+                    <TransactionLockupFailed
+                        setStatusOverride={setStatusOverride}
+                    />
+                </>
+            ),
+            {
+                wrapper: contextWrapper,
+            },
+        );
+        payContext.setSwap({
+            assetReceive: BTC,
+            version: OutputType.Taproot,
+            status: swapStatusFailed.SwapExpired,
+        } as SomeSwap);
+
+        await expect(
+            screen.findByText(i18n.en.no_lockup_transaction),
         ).resolves.not.toBeUndefined();
     });
 });
