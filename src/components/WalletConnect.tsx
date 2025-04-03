373619a9cb57ae0c8990da779ce4b1469e660481
michael1011 <me@michael1011.at>
2025-04-03 23:49:21 +0200

    fix: rollback WalletConnect to fix production builds (#882)

--- src/components/WalletConnect.tsx
+++ src/components/WalletConnect.tsx
@@ -64,7 +64,7 @@ export const WalletConnect = () => {
             features: {
                 email: false,
                 socials: false,
-                analytics: true,
+                analytics: false,
             },
         });
 
