BoltzExchange/boltz-web-app

commit cdee6e7da7e8c3095ea623598f8c33cbf88109de

feat: claim pending chain/reverse swaps with rescue key (#973)
* chore: update regtest submodule

* feat: Rename "Refund" page to "Rescue" page

* feat: Claim pending chain/reverse swaps via rescue key scan

* feat: Derive preimage from rescue key

With this users are able to derive preimages from past pending swaps and
claim them using their saved rescue key.

* fix: sort rescuable swaps by date

* fix: undefined error logging

* test(e2e): Rename "Refund" page to "Rescue"

* test(e2e): Claim pending chain swap via rescue key scan

* refactor: Improve error handling for `claim.ts`

* test(e2e): Support running commands in background

* chore: update i18n translations

* chore: Replace RescuableSwap with RestorableSwap

* fixup! feat: Claim pending chain/reverse swaps via rescue key scan

* fixup! feat: Derive preimage from rescue key

* fixup! test(e2e): Rename "Refund" page to "Rescue"

* fixup! chore: update i18n translations

* fixup! feat: Claim pending chain/reverse swaps via rescue key scan

* chore: cleanup i18n

* chore: minor fixes

---------

Co-authored-by: michael1011 <me@michael1011.at>

src/components/SwapChecker.tsx
@@ -25,6 +25,7 @@ import {
 } from "../utils/claim";
 import { formatError } from "../utils/errors";
 import { getApiUrl, getPair } from "../utils/helper";
+import { isSwapClaimable } from "../utils/rescue";
 import type {
     ChainSwap,
     ReverseSwap,
@@ -270,18 +271,7 @@ export const SwapChecker = () => {
         if (
             currentSwap.claimTx === undefined &&
             data.transaction !== undefined &&
-            ((currentSwap.type === SwapType.Reverse &&
-                [
-                    swapStatusPending.TransactionConfirmed,
-                    swapStatusPending.TransactionMempool,
-                    swapStatusSuccess.InvoiceSettled,
-                ].includes(data.status)) ||
-                (currentSwap.type === SwapType.Chain &&
-                    [
-                        swapStatusSuccess.TransactionClaimed,
-                        swapStatusPending.TransactionServerConfirmed,
-                        swapStatusPending.TransactionServerMempool,
-                    ].includes(data.status)))
+            isSwapClaimable(data.status, currentSwap.type)
         ) {
             try {
                 const res = await claim(

src/utils/rescue.ts
@@ -32,6 +32,27 @@ import type { ChainSwap, SomeSwap, SubmarineSwap } from "./swapCreator";
 import { isRsk } from "./swapCreator";
 import { createMusig, hashForWitnessV1, tweakMusig } from "./taproot/musig";
 
+export enum RescueAction {
+    None = "none",
+    Claim = "claim",
+    Refund = "refund",
+    Pending = "pending",
+}
+
+export const RescueNoAction = [RescueAction.None, RescueAction.Pending];
+
+export const isSwapClaimable = (status: string, type: SwapType) =>
+    (type === SwapType.Reverse &&
+        [
+            swapStatusPending.TransactionConfirmed,
+            swapStatusPending.TransactionMempool,
+        ].includes(status)) ||
+    (type === SwapType.Chain &&
+        [
+            swapStatusPending.TransactionServerConfirmed,
+            swapStatusPending.TransactionServerMempool,
+        ].includes(status));
+
 const refundTaproot = async <T extends TransactionInterface>(
     swap: SubmarineSwap | ChainSwap,
     lockupTxs: TransactionInterface[],
@@ -294,7 +315,7 @@ export const getRefundableUTXOs = async (currentSwap: SomeSwap) => {
     };
 
     if (!isSwapRefundable(currentSwap)) {
-        log.warn(`swap ${currentSwap.id} is not refundable`);
+        log.debug(`swap ${currentSwap.id} is not refundable`);
         return [];
     }
 
@@ -318,24 +339,35 @@ export const getRefundableUTXOs = async (currentSwap: SomeSwap) => {
     }
 
     // if both requests were "rejected"
-    log.error("failed to fetch utxo data for swap: ", currentSwap.id);
+    log.error("failed to fetch utxo data for swap:", currentSwap.id);
     return [];
 };
 
-export const createRefundList = async (swaps: SomeSwap[]) => {
+export const createRescueList = async (swaps: SomeSwap[]) => {
     return await Promise.all(
         swaps.map(async (swap) => {
             try {
+                if (isSwapClaimable(swap.status, swap.type)) {
+                    return { ...swap, action: RescueAction.Claim };
+                }
+
+                if (Object.values(swapStatusPending).includes(swap.status)) {
+                    return { ...swap, action: RescueAction.Pending };
+                }
+
                 const utxos = await getRefundableUTXOs(swap);
 
                 if (utxos.length > 0) {
-                    return swap;
+                    return { ...swap, action: RescueAction.Refund };
                 }
 
-                return { ...swap, disabled: true };
+                return { ...swap, action: RescueAction.None };
             } catch (e) {
-                log.error("error creating refund list: ", e.stack);
-                return { ...swap, disabled: true };
+                log.error(
+                    `error creating rescue list for swap ${swap.id}:`,
+                    formatError(e),
+                );
+                return { ...swap, action: RescueAction.None };
             }
         }),
     );

e2e/boltzClient.ts
@@ -3,16 +3,16 @@ import { mnemonicToSeedSync } from "@scure/bip39";
 import axios from "axios";
 import fs from "fs";
 
-import type { RescuableSwap } from "../src/utils/boltzClient";
+import type { RestorableSwap } from "../src/utils/boltzClient";
 
 const boltzEndpoint = "http://localhost:9001";
 
-export const getRescuableSwaps = async (rescueFile: string) => {
+export const getRestorableSwaps = async (rescueFile: string) => {
     const mnemonic = JSON.parse(fs.readFileSync(rescueFile, "utf8")).mnemonic;
     const seed = mnemonicToSeedSync(mnemonic);
 
     return (
-        await axios.post<RescuableSwap[]>(`${boltzEndpoint}/v2/swap/rescue`, {
+        await axios.post<RestorableSwap[]>(`${boltzEndpoint}/v2/swap/restore`, {
             xpub: HDKey.fromMasterSeed(seed).publicExtendedKey,
         })
     ).data;

e2e/rescue/claim.spec.ts
@@ -1,0 +1,250 @@
+import { expect, test } from "@playwright/test";
+import type { Page } from "@playwright/test";
+import fs from "fs";
+
+import { BTC, LBTC } from "../../src/consts/Assets";
+import dict from "../../src/i18n/i18n";
+import {
+    bitcoinSendToAddress,
+    elementsSendToAddress,
+    generateBitcoinBlock,
+    generateLiquidBlock,
+    getBitcoinAddress,
+    getLiquidAddress,
+    payInvoiceLndBackground,
+    waitForNodesToSync,
+} from "../utils";
+
+const fileName = "rescue-file.json";
+
+const clearStorage = async (page: Page) => {
+    await page.evaluate(() => window.localStorage.clear());
+    await page.reload();
+};
+
+const backupRescueFile = async (page: Page) => {
+    const downloadPromise = page.waitForEvent("download");
+    await page.getByRole("button", { name: dict.en.download_new_key }).click();
+
+    await (await downloadPromise).saveAs(fileName);
+
+    await page.getByTestId("rescueFileUpload").setInputFiles(fileName);
+};
+
+const claimPendingSwap = async ({
+    page,
+    asset,
+    swapId,
+}: {
+    page: Page;
+    asset: string;
+    swapId: string;
+}) => {
+    await page.getByRole("link", { name: "Rescue" }).click();
+
+    await page.getByRole("button", { name: "Rescue external swap" }).click();
+
+    await page.getByTestId("refundUpload").setInputFiles(fileName);
+
+    const swapItem = page.locator(`div[data-testid='swaplist-item-${swapId}']`);
+
+    await expect(swapItem).toBeVisible();
+    await expect(swapItem).not.toBeDisabled();
+
+    await swapItem.click();
+
+    const inputOnchainAddress = page.locator(
+        "input[data-testid='onchainAddress']",
+    );
+
+    await inputOnchainAddress.fill(
+        asset === BTC ? await getBitcoinAddress() : await getLiquidAddress(),
+    );
+
+    await page.getByRole("button", { name: "Claim" }).click();
+
+    await expect(page.getByTestId("claimed")).toBeVisible();
+};
+
+const getCurrentSwapId = (page: Page) => {
+    const url = new URL(page.url());
+    return url.pathname.split("/").pop();
+};
+
+const createChainSwap = async (page: Page, assetSend: string) => {
+    await page.goto("/");
+
+    const assetSelector = page.locator("div[class='asset asset-LN'] div");
+    await assetSelector.click();
+
+    const lbtcAsset = page.locator("div[data-testid='select-L-BTC']");
+    await lbtcAsset.click();
+
+    if (assetSend === BTC) {
+        const divFlipAssets = page.locator("#flip-assets");
+        await divFlipAssets.click();
+    }
+
+    const receiveAmount = "0.01";
+    const inputReceiveAmount = page.locator(
+        "input[data-testid='receiveAmount']",
+    );
+    await inputReceiveAmount.fill(receiveAmount);
+
+    const inputSendAmount = page
+        .locator("input[data-testid='sendAmount']")
+        .inputValue();
+
+    const inputOnchainAddress = page.locator(
+        "input[data-testid='onchainAddress']",
+    );
+    await inputOnchainAddress.fill(
+        assetSend === BTC
+            ? await getLiquidAddress()
+            : await getBitcoinAddress(),
+    );
+
+    const buttonCreateSwap = page.locator(
+        "button[data-testid='create-swap-button']",
+    );
+    await buttonCreateSwap.click();
+
+    return inputSendAmount;
+};
+
+test.describe("Claim", () => {
+    test.beforeEach(async () => {
+        await generateBitcoinBlock();
+        await generateLiquidBlock();
+    });
+
+    test.afterEach(() => {
+        if (fs.existsSync(fileName)) {
+            fs.unlinkSync(fileName);
+        }
+    });
+
+    [
+        { assetSend: BTC, assetReceive: LBTC },
+        { assetSend: LBTC, assetReceive: BTC },
+    ].forEach(({ assetSend, assetReceive }) => {
+        test(`${assetSend} -> ${assetReceive}: Claim pending chain swap via rescue key scan`, async ({
+            page,
+        }) => {
+            const sendAmount = await createChainSwap(page, assetSend);
+
+            await backupRescueFile(page);
+
+            await page.locator("p[data-testid='copy-box']").click();
+
+            const address = await page.evaluate(() => {
+                return navigator.clipboard.readText();
+            });
+
+            const swapId = getCurrentSwapId(page);
+
+            await clearStorage(page);
+
+            if (assetSend === BTC) {
+                await bitcoinSendToAddress(address, sendAmount);
+                await generateBitcoinBlock();
+                await waitForNodesToSync();
+            }
+
+            if (assetSend === LBTC) {
+                await elementsSendToAddress(address, sendAmount);
+                await generateLiquidBlock();
+                await waitForNodesToSync();
+            }
+
+            await claimPendingSwap({
+                page,
+                swapId,
+                asset: assetReceive,
+            });
+        });
+    });
+
+    [BTC, LBTC].forEach((asset) => {
+        test(`${asset}: Claim pending reverse swap via rescue key scan`, async ({
+            page,
+        }) => {
+            await page.goto("/");
+
+            const receiveAmount = "0.01";
+            const inputReceiveAmount = page.locator(
+                "input[data-testid='receiveAmount']",
+            );
+            await inputReceiveAmount.fill(receiveAmount);
+
+            const inputOnchainAddress = page.locator(
+                "input[data-testid='onchainAddress']",
+            );
+
+            if (asset === BTC) {
+                await inputOnchainAddress.fill(await getBitcoinAddress());
+            }
+
+            if (asset === LBTC) {
+                const assetSelector = page.locator(
+                    "div[class='asset asset-BTC'] div",
+                );
+                await assetSelector.click();
+
+                const lbtcAsset = page.locator(
+                    "div[data-testid='select-L-BTC']",
+                );
+                await lbtcAsset.click();
+
+                await inputOnchainAddress.fill(await getLiquidAddress());
+            }
+
+            const buttonCreateSwap = page.locator(
+                "button[data-testid='create-swap-button']",
+            );
+            await buttonCreateSwap.click();
+
+            const spanLightningInvoice = page.locator("span[class='btn']");
+            await spanLightningInvoice.click();
+
+            const lightningInvoice = await page.evaluate(() => {
+                return navigator.clipboard.readText();
+            });
+            expect(lightningInvoice).toBeDefined();
+
+            const swapId = getCurrentSwapId(page);
+
+            const settings = page.locator("span[data-testid='settings-cog']");
+
+            await settings.click();
+
+            const downloadPromise = page.waitForEvent("download");
+
+            await page
+                .locator("div[data-testid='rescue-key-download']")
+                .click();
+            await (await downloadPromise).saveAs(fileName);
+
+            await clearStorage(page);
+
+            payInvoiceLndBackground(lightningInvoice);
+            await waitForNodesToSync();
+
+            if (asset === BTC) {
+                await generateBitcoinBlock();
+                await waitForNodesToSync();
+            }
+
+            if (asset === LBTC) {
+                await generateLiquidBlock();
+                await waitForNodesToSync();
+            }
+
+            await claimPendingSwap({
+                page,
+                swapId,
+                asset,
+            });
+        });
+    });
+});

e2e/rescue/refund.spec.ts
@@ -38,7 +38,7 @@ const waitForUTXOsInMempool = async (address: string, amount: number) => {
 };
 
 const navigateToSwapDetails = async (page: Page, swapId: string) => {
-    await page.getByRole("link", { name: "Refund" }).click();
+    await page.getByRole("link", { name: "Rescue" }).click();
     const swapItem = page.locator(`div[data-testid='swaplist-item-${swapId}']`);
     await expect(page.getByTestId("loading-spinner")).not.toBeVisible();
     await expect(swapItem.getByRole("link", { name: "Refund" })).toBeVisible();

e2e/rescue/refundFile.spec.ts
@@ -9,9 +9,9 @@ test.describe("Refund files", () => {
     }) => {
         await page.goto("/");
 
-        await page.getByRole("link", { name: "Refund" }).click();
+        await page.getByRole("link", { name: "Rescue" }).click();
         await page
-            .getByRole("button", { name: "Refund external swap" })
+            .getByRole("button", { name: "Rescue external swap" })
             .click();
         await page.getByTestId("refundUpload").click();
 

e2e/rescue/rescueFile.spec.ts
@@ -6,7 +6,7 @@ import path from "path";
 import { config } from "../../src/config";
 import dict from "../../src/i18n/i18n";
 import type { UTXO } from "../../src/utils/blockchain";
-import { getRescuableSwaps } from "../boltzClient";
+import { getRestorableSwaps } from "../boltzClient";
 import {
     createAndVerifySwap,
     elementsSendToAddress,
@@ -50,13 +50,13 @@ test.describe("Rescue file", () => {
     test("should create swap after new backup download", async ({ page }) => {
         await createAndVerifySwap(page, rescueFileJson);
         // Verify that the swap was created
-        expect(await getRescuableSwaps(rescueFileJson)).toHaveLength(1);
+        expect(await getRestorableSwaps(rescueFileJson)).toHaveLength(1);
     });
 
     test("should create swap after existing backup verification", async ({
         page,
     }) => {
-        const existingSwaps = await getRescuableSwaps(existingFilePath);
+        const existingSwaps = await getRestorableSwaps(existingFilePath);
 
         await page.goto("/");
         await setupSwapAssets(page);
@@ -71,7 +71,7 @@ test.describe("Rescue file", () => {
         await page.getByText("address").click();
 
         // Verify that a new swap was created
-        expect(await getRescuableSwaps(existingFilePath)).toHaveLength(
+        expect(await getRestorableSwaps(existingFilePath)).toHaveLength(
             existingSwaps.length + 1,
         );
     });
@@ -113,8 +113,8 @@ test.describe("Rescue file", () => {
             .setInputFiles(existingFilePath);
         await page.getByText("address").click();
 
-        const swaps = await getRescuableSwaps(existingFilePath);
-        const keyIndices = swaps.map((swap) => swap.keyIndex);
+        const swaps = await getRestorableSwaps(existingFilePath);
+        const keyIndices = swaps.map((swap) => swap.refundDetails?.keyIndex);
         for (let i = 0; i < keyIndices.length - 1; i++) {
             expect(keyIndices[i + 1]).toEqual(keyIndices[i] + 1);
         }
@@ -125,9 +125,9 @@ test.describe("Rescue file", () => {
     }) => {
         await createAndVerifySwap(page, rescueFileJson);
 
-        await page.getByRole("link", { name: "Refund" }).click();
+        await page.getByRole("link", { name: "Rescue" }).click();
         await page
-            .getByRole("button", { name: "Refund External Swap" })
+            .getByRole("button", { name: "Rescue external swap" })
             .click();
 
         await page.getByTestId("refundUpload").setInputFiles(rescueFileJson);
@@ -176,9 +176,9 @@ test.describe("Rescue file", () => {
             )
             .toBe(true);
 
-        await page.getByRole("link", { name: "Refund" }).click();
+        await page.getByRole("link", { name: "Rescue" }).click();
         await page
-            .getByRole("button", { name: "Refund External Swap" })
+            .getByRole("button", { name: "Rescue external swap" })
             .click();
 
         await page.getByTestId("refundUpload").setInputFiles(rescueFileJson);

commit aef7f8d963813760ebe8e1bee018d903c7fe3eaa

fix: Restore previous `SwapChecker` behavior
A regression was introduced in #973, replacing an existing
condition in `SwapChecker` with `isSwapClaimable`, but `isSwapClaimable`
did not include all swap status which were prevoiusly defined in
`SwapChecker`.

src/components/SwapChecker.tsx
@@ -271,7 +271,11 @@ export const SwapChecker = () => {
         if (
             currentSwap.claimTx === undefined &&
             data.transaction !== undefined &&
-            isSwapClaimable(data.status, currentSwap.type)
+            isSwapClaimable({
+                status: data.status,
+                type: currentSwap.type,
+                includeSuccess: true,
+            })
         ) {
             try {
                 const res = await claim(

src/utils/rescue.ts
@@ -8,7 +8,7 @@ import log from "loglevel";
 
 import { LBTC } from "../consts/Assets";
 import { SwapType } from "../consts/Enums";
-import { swapStatusPending } from "../consts/SwapStatus";
+import { swapStatusPending, swapStatusSuccess } from "../consts/SwapStatus";
 import type { deriveKeyFn } from "../context/Global";
 import secp from "../lazy/secp";
 import { getSwapUTXOs } from "./blockchain";
@@ -41,17 +41,44 @@ export enum RescueAction {
 
 export const RescueNoAction = [RescueAction.None, RescueAction.Pending];
 
-export const isSwapClaimable = (status: string, type: SwapType) =>
-    (type === SwapType.Reverse &&
-        [
-            swapStatusPending.TransactionConfirmed,
-            swapStatusPending.TransactionMempool,
-        ].includes(status)) ||
-    (type === SwapType.Chain &&
-        [
-            swapStatusPending.TransactionServerConfirmed,
-            swapStatusPending.TransactionServerMempool,
-        ].includes(status));
+export const isSwapClaimable = ({
+    status,
+    type,
+    includeSuccess = false,
+}: {
+    status: string;
+    type: SwapType;
+    includeSuccess?: boolean;
+}) => {
+    switch (type) {
+        case SwapType.Reverse: {
+            const statuses = [
+                swapStatusPending.TransactionConfirmed,
+                swapStatusPending.TransactionMempool,
+            ];
+
+            if (includeSuccess) {
+                statuses.push(swapStatusSuccess.InvoiceSettled);
+            }
+
+            return statuses.includes(status);
+        }
+        case SwapType.Chain: {
+            const statuses = [
+                swapStatusPending.TransactionServerConfirmed,
+                swapStatusPending.TransactionServerMempool,
+            ];
+
+            if (includeSuccess) {
+                statuses.push(swapStatusSuccess.TransactionClaimed);
+            }
+
+            return statuses.includes(status);
+        }
+        default:
+            return false;
+    }
+};
 
 const refundTaproot = async <T extends TransactionInterface>(
     swap: SubmarineSwap | ChainSwap,
@@ -347,7 +374,12 @@ export const createRescueList = async (swaps: SomeSwap[]) => {
     return await Promise.all(
         swaps.map(async (swap) => {
             try {
-                if (isSwapClaimable(swap.status, swap.type)) {
+                if (
+                    isSwapClaimable({
+                        status: swap.status,
+                        type: swap.type,
+                    })
+                ) {
                     return { ...swap, action: RescueAction.Claim };
                 }
 