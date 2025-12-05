92f9ae63d817647b17bbf15a9bd840fd7003df9c
Caio <c@bol.tz>
2025-12-05 16:00:13 -0300

    chore: optimize e2e github actions (#1098)
    * chore: use playwright shards on github actions
    
    * chore: start only necessary regtest containers
    
    * chore: update actions to v5

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: "html",
+    reporter: process.env.CI ? "blob" : "html",
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
@@ -31,6 +31,7 @@ export default defineConfig({
     projects: [
         {
             name: "chromium",
+            fullyParallel: true,
             use: {
                 ...devices["Desktop Chrome"],
                 contextOptions: {
