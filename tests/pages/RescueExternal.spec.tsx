36b26567158ab74d00d15e10217d7a774c394847
Caio <c@bol.tz>
2026-01-08 18:21:08 -0300

    fixup! feat: progressive swap restoration on external rescue

--- tests/pages/RescueExternal.spec.tsx
+++ tests/pages/RescueExternal.spec.tsx
@@ -265,6 +265,8 @@ describe("RescueExternal", () => {
                         }),
                 );
 
+                mockGetRestorableSwaps.mockResolvedValueOnce([]);
+
                 render(
                     () => (
                         <>
