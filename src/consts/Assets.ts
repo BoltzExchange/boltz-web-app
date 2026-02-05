28f48aabcf8ee62b671add30d042dd1eb937fda2
Caio <c@bol.tz>
2026-02-05 18:40:57 -0300

    refactor: update refundable and evm assets types

--- src/consts/Assets.ts
+++ src/consts/Assets.ts
@@ -4,11 +4,11 @@ export const LBTC = "L-BTC";
 export const RBTC = "RBTC";
 
 export type AssetType = typeof LN | typeof BTC | typeof LBTC | typeof RBTC;
-export type RefundableAssetType = typeof BTC | typeof LBTC;
+export type RefundableAssetType = typeof BTC | typeof LBTC | typeof RBTC;
 
 export const assets = [LN, BTC, LBTC, RBTC];
 
-export const refundableAssets = [BTC, LBTC];
+export const refundableAssets = [BTC, LBTC, RBTC];
 
 export const btcChains = [BTC, LBTC];
 
