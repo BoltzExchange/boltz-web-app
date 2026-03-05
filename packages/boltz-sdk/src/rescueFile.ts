fd82cc0ed3978a63deeccd5a46f77cabea89e77a
Caio <c@bol.tz>
2026-03-05 03:33:45 -0300

    chore: add rescue file utils to boltz-sdk

---
+++ packages/boltz-sdk/src/rescueFile.ts
@@ -0,0 +1,83 @@
+import { sha256 } from "@noble/hashes/sha2.js";
+import { HDKey } from "@scure/bip32";
+import {
+    generateMnemonic,
+    mnemonicToSeedSync,
+    validateMnemonic,
+} from "@scure/bip39";
+import { wordlist } from "@scure/bip39/wordlists/english.js";
+
+import { type AssetType, RBTC } from "./assets";
+
+export enum Errors {
+    InvalidFile = "invalid file",
+    NotAllElementsHaveAnId = "not all elements have an id",
+    InvalidMnemonic = "invalid mnemonic",
+}
+
+export type RescueFile = {
+    mnemonic: string;
+};
+
+export const derivationPath = "m/44/0/0/0";
+export const rskDerivationPath = "m/44/137/0/0";
+
+const getPath = (index: number) => `${derivationPath}/${index}`;
+
+const getRskPath = (index: number) => `${rskDerivationPath}/${index}`;
+
+export const mnemonicToHDKey = (mnemonic: string) => {
+    const seed = mnemonicToSeedSync(mnemonic);
+    return HDKey.fromMasterSeed(seed);
+};
+
+export const getXpub = (rescueFile: RescueFile) => {
+    return mnemonicToHDKey(rescueFile.mnemonic).publicExtendedKey;
+};
+
+export const generateRescueFile = (): RescueFile => ({
+    mnemonic: generateMnemonic(wordlist),
+});
+
+export const deriveKey = (
+    rescueFile: RescueFile,
+    index: number,
+    asset: AssetType,
+    hdKey?: HDKey,
+) => {
+    const derivationPath = asset === RBTC ? getRskPath(index) : getPath(index);
+    if (!hdKey) {
+        return mnemonicToHDKey(rescueFile.mnemonic).derive(derivationPath);
+    }
+    return hdKey.derive(derivationPath);
+};
+
+export const validateRescueFile = (
+    data: Record<string, string | object | number | boolean>,
+): RescueFile => {
+    if (!("mnemonic" in data)) {
+        throw Errors.InvalidFile;
+    }
+
+    if (!validateMnemonic(data.mnemonic as string, wordlist)) {
+        throw Errors.InvalidMnemonic;
+    }
+
+    getXpub(data as RescueFile);
+
+    return data as RescueFile;
+};
+
+export const derivePreimageFromRescueKey = (
+    rescueKey: RescueFile,
+    keyIndex: number,
+    asset: AssetType,
+    hdKey?: HDKey,
+): Uint8Array => {
+    const privateKey = deriveKey(rescueKey, keyIndex, asset, hdKey).privateKey;
+    if (!privateKey) {
+        throw new Error("failed to derive private key for preimage");
+    }
+
+    return sha256(privateKey);
+};
