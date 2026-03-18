613fd813c8ccd3eee624a35633cf76bc482db034
michael1011 <me@michael1011.at>
2026-03-18 11:31:37 +0100

    refactor: show OFT transaction link for refunds

--- src/status/SwapRefunded.tsx
+++ src/status/SwapRefunded.tsx
@@ -1,21 +1,29 @@
 import { useNavigate } from "@solidjs/router";
 
 import BlockExplorer from "../components/BlockExplorer";
+import { config } from "../config";
 import { useGlobalContext } from "../context/Global";
 import { usePayContext } from "../context/Pay";
+import { OftPosition } from "../utils/swapCreator";
 
 const SwapRefunded = (props: { refundTxId: string }) => {
     const navigate = useNavigate();
     const { swap } = usePayContext();
     const { t } = useGlobalContext();
+    const isPreOft = () => swap().oft?.position === OftPosition.Pre;
 
     return (
         <div>
             <p>{t("refunded")}</p>
             <hr />
             <BlockExplorer
-                asset={swap().assetSend}
+                asset={isPreOft() ? swap().oft.sourceAsset : swap().assetSend}
                 txId={props.refundTxId}
+                href={
+                    isPreOft()
+                        ? `${config.layerZeroExplorerUrl}/tx/${props.refundTxId}`
+                        : undefined
+                }
                 typeLabel="refund_tx"
             />
             <hr />
