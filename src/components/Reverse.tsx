67a1a5813e7b7989d9919d948584f28a462eb8f0
C <c@bol.tz>
2026-03-24 16:58:00 -0300

    feat: flip amount and assets for USDT swaps (#1277)

--- src/components/Reverse.tsx
+++ src/components/Reverse.tsx
@@ -1,14 +1,40 @@
 import { ImArrowDown } from "solid-icons/im";
 
+import { Side } from "../consts/Enums";
 import { useCreateContext } from "../context/Create";
 import { useGlobalContext } from "../context/Global";
 import Pair from "../utils/Pair";
+import { getDecimals } from "../utils/denomination";
 
 const Reverse = () => {
     const { pairs, regularPairs } = useGlobalContext();
-    const { pair, setPair, setOnchainAddress, setInvoice } = useCreateContext();
+    const {
+        pair,
+        setPair,
+        setOnchainAddress,
+        setInvoice,
+        sendAmount,
+        setSendAmount,
+        receiveAmount,
+        setReceiveAmount,
+        amountChanged,
+        setAmountChanged,
+    } = useCreateContext();
 
     const setDirection = () => {
+        const fromErc20 = getDecimals(pair().fromAsset).isErc20;
+        const toErc20 = getDecimals(pair().toAsset).isErc20;
+
+        if (fromErc20 || toErc20) {
+            const prevSend = sendAmount();
+            const prevReceive = receiveAmount();
+            setSendAmount(prevReceive);
+            setReceiveAmount(prevSend);
+            setAmountChanged(
+                amountChanged() === Side.Send ? Side.Receive : Side.Send,
+            );
+        }
+
         setOnchainAddress("");
         setInvoice("");
         setPair(
