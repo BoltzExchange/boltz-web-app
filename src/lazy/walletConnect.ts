7bfe0b4682071e1102cd5fe752fec63c8e196131
jackstar12 <j@bol.tz>
2026-04-22 09:06:53 +0200

    feat: add metamask and tronlink

--- src/lazy/walletConnect.ts
+++ src/lazy/walletConnect.ts
@@ -1,31 +1,48 @@
 import Loader from "./Loader";
 
 export default new Loader("WalletConnect", async () => {
-    const [appKit, EthersAdapter, SolanaAdapter, TronAdapter, networks] =
-        await Promise.all([
-            import("@reown/appkit"),
-            (async () => {
-                const ethersAdapter =
-                    await import("@reown/appkit-adapter-ethers");
-                return ethersAdapter.EthersAdapter;
-            })(),
-            (async () => {
-                const solanaAdapter =
-                    await import("@reown/appkit-adapter-solana");
-                return solanaAdapter.SolanaAdapter;
-            })(),
-            (async () => {
-                const tronAdapter = await import("@reown/appkit-adapter-tron");
-                return tronAdapter.TronAdapter;
-            })(),
-            import("@reown/appkit/networks"),
-        ]);
+    const [
+        appKit,
+        EthersAdapter,
+        SolanaAdapter,
+        TronAdapter,
+        MetaMaskAdapter,
+        TronLinkAdapter,
+        networks,
+    ] = await Promise.all([
+        import("@reown/appkit"),
+        (async () => {
+            const ethersAdapter = await import("@reown/appkit-adapter-ethers");
+            return ethersAdapter.EthersAdapter;
+        })(),
+        (async () => {
+            const solanaAdapter = await import("@reown/appkit-adapter-solana");
+            return solanaAdapter.SolanaAdapter;
+        })(),
+        (async () => {
+            const tronAdapter = await import("@reown/appkit-adapter-tron");
+            return tronAdapter.TronAdapter;
+        })(),
+        (async () => {
+            const metaMaskAdapter =
+                await import("@tronweb3/tronwallet-adapter-metamask-tron");
+            return metaMaskAdapter.MetaMaskAdapter;
+        })(),
+        (async () => {
+            const tronLinkAdapter =
+                await import("@tronweb3/tronwallet-adapter-tronlink");
+            return tronLinkAdapter.TronLinkAdapter;
+        })(),
+        import("@reown/appkit/networks"),
+    ]);
 
     return {
         appKit,
         EthersAdapter,
         SolanaAdapter,
         TronAdapter,
+        MetaMaskAdapter,
+        TronLinkAdapter,
         solana: networks.solana,
         tronMainnet: networks.tronMainnet,
     };
