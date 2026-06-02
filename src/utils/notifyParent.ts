032b96fcba39881f45f021e7bb8fd58b71a34208
tompro <office@protom.eu>
2026-06-02 15:11:57 +0200

    Review fixes

--- src/utils/notifyParent.ts
+++ src/utils/notifyParent.ts
@@ -1,15 +1,27 @@
+import log from "loglevel";
+
 import { useGlobalContext } from "../context/Global";
+import { formatError } from "../utils/errors";
 
 export const useParentNotifier = () => {
     const { embeddedMode, parentOrigin } = useGlobalContext();
 
     const notifyParent = (message: unknown) => {
         const origin = parentOrigin();
-        if (!embeddedMode() || !origin || window.parent === window) {
+        if (
+            !embeddedMode() ||
+            !origin ||
+            origin === "*" ||
+            window.parent === window
+        ) {
             return;
         }
 
-        window.parent.postMessage(message, origin);
+        try {
+            window.parent.postMessage(message, origin);
+        } catch (e) {
+            log.error("failed to notify parent window", formatError(e));
+        }
     };
 
     return { notifyParent };
